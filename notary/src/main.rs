2927e2a69cc7eabb03537746901180b819286caf
mattes <1240531+mattes@users.noreply.github.com>
2024-07-23 11:17:42 -0700

    add a few todos

--- notary/src/main.rs
+++ notary/src/main.rs
@@ -1,3 +1,17 @@
+// TODO combined binary which offers the following endpoints:
+//
+// GET /health
+//
+// GET /v1/origo/proxy (supports TCP or websocket connection) - focus on this one later, requires Thor's work.
+//
+// POST /v1/tlsnotary/session -> https://github.com/tlsnotary/tlsn/blob/3554db83e17b2e5fc98293b397a2907b7f023496/notary/server/src/service.rs#L114 (pub async fn initialize)
+// GET /v1/tlsnotary/notarize (supports TCP or websocket connection)
+//    a few possible entrypoints:
+//       - https://github.com/tlsnotary/tlsn/blob/3554db83e17b2e5fc98293b397a2907b7f023496/notary/server/src/service.rs#L71 (pub async fn upgrade_protocol)
+//       - https://github.com/tlsnotary/tlsn/blob/3554db83e17b2e5fc98293b397a2907b7f023496/notary/server/src/service.rs#L173 (pub async fn notary_service)
+//
+// GET /v1/tlsnotary/proxy (mostly this: https://github.com/pluto/web-prover/blob/30ba86a2d5887c2f7c4e2d7bb50b378998ccd297/bin/proxy.rs#L219)
+
 fn main() {
   println!("Hello, world!");
 }
