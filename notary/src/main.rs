0bf275c67cc897802dc8aff3ac0f1df5e168dd71
mattes <1240531+mattes@users.noreply.github.com>
2024-07-23 13:52:44 -0700

    fix tracing

--- notary/src/main.rs
+++ notary/src/main.rs
@@ -15,12 +15,7 @@
 //
 // GET /v1/tlsnotary/proxy (mostly this: https://github.com/pluto/web-prover/blob/30ba86a2d5887c2f7c4e2d7bb50b378998ccd297/bin/proxy.rs#L219)
 
-use std::{
-  fs, io,
-  net::{IpAddr, SocketAddr},
-  pin::Pin,
-  sync::Arc,
-};
+use std::{fs, io, sync::Arc};
 
 use axum::{
   extract::Request,
@@ -30,7 +25,6 @@ use axum::{
   routing::{get, post},
   Json, Router,
 };
-use futures_util::future::poll_fn;
 use hyper::{body::Incoming, server::conn::http1};
 use hyper_util::rt::TokioIo;
 use rustls::{
@@ -41,25 +35,24 @@ use tokio::net::TcpListener;
 use tokio_rustls::TlsAcceptor;
 // use tower_http::cors::CorsLayer;
 use tower_service::Service;
-use tracing::{debug, info};
+use tracing::{debug, error, info};
 
 #[tokio::main]
 async fn main() {
-  let certs = load_certs("./fixture/certs/server-cert.pem").unwrap();
-  let key = load_private_key("./fixture/certs/server-key.pem").unwrap();
+  let subscriber = tracing_subscriber::FmtSubscriber::new();
+  tracing::subscriber::set_global_default(subscriber).unwrap();
+
+  let certs = load_certs("./fixture/certs/server-cert.pem").unwrap(); // TODO make CLI or ENV var
+  let key = load_private_key("./fixture/certs/server-key.pem").unwrap(); // TODO make CLI or ENV var
+  let addr = "127.0.0.1:8070"; // TODO make env var?
+
+  let listener = TcpListener::bind(addr).await.unwrap();
+  info!("Listening on https://{}", addr);
 
   let mut server_config =
     ServerConfig::builder().with_no_client_auth().with_single_cert(certs, key).unwrap();
-
-  // Set the http protocols we support
   server_config.alpn_protocols = vec![b"http/1.1".to_vec()];
-  let tls_config = Arc::new(server_config);
-  let tls_acceptor = TlsAcceptor::from(tls_config);
-
-  let notary_address = SocketAddr::new(IpAddr::V4("0.0.0.0".parse().unwrap()), 7074);
-  let mut listener = TcpListener::bind(notary_address).await.unwrap();
-
-  info!("Listening for TCP traffic at {}", notary_address);
+  let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
 
   let protocol = Arc::new(http1::Builder::new());
 
@@ -67,42 +60,23 @@ async fn main() {
     Router::new().route("/health", get(|| async move { (StatusCode::OK, "Ok").into_response() }));
 
   loop {
-    // Poll and await for any incoming connection, ensure that all operations inside are infallible
-    // to prevent bringing down the server
-    let stream = match poll_fn(|cx| Pin::new(&mut listener).poll_accept(cx)).await {
-      Ok((stream, _)) => stream,
-      Err(err) => {
-        panic!("todo");
-        // error!("{}", NotaryServerError::Connection(err.to_string()));
-        // continue;
-      },
-    };
-    debug!("Received a TCP connection");
-
-    let tower_service = router.clone();
+    let (tcp_stream, _) = listener.accept().await.unwrap();
     let tls_acceptor = tls_acceptor.clone();
+    let tower_service = router.clone();
     let protocol = protocol.clone();
 
     tokio::spawn(async move {
-      match tls_acceptor.accept(stream).await {
-        Ok(stream) => {
-          info!("Accepted prover's TLS-secured TCP connection");
-          // Reference: https://github.com/tokio-rs/axum/blob/5201798d4e4d4759c208ef83e30ce85820c07baa/examples/low-level-rustls/src/main.rs#L67-L80
-          let io = TokioIo::new(stream);
+      match tls_acceptor.accept(tcp_stream).await {
+        Ok(tls_stream) => {
+          let io = TokioIo::new(tls_stream);
           let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
             tower_service.clone().call(request)
           });
-          // Serve different requests using the same hyper protocol and axum router
-          let _ = protocol
-                  .serve_connection(io, hyper_service)
-                  // use with_upgrades to upgrade connection to websocket for websocket clients
-                  // and to extract tcp connection for tcp clients
-                  .with_upgrades()
-                  .await;
+          // TODO should we check returned Result here?
+          let _ = protocol.serve_connection(io, hyper_service).with_upgrades().await;
         },
         Err(err) => {
-          panic!("todo");
-          // error!("{}", NotaryServerError::Connection(err.to_string()));
+          error!("{err:#}"); // TODO format this better
         },
       }
     });
