eb8fdd4cd074f17de9cb0d2057fbde22b10c4995
mattes <1240531+mattes@users.noreply.github.com>
2024-08-02 11:37:15 -0700

    wasm client for alpha4

--- client_wasm/demo/js/index.js
+++ client_wasm/demo/js/index.js
@@ -12,38 +12,39 @@ setup_tracing("debug,tlsn_extension_rs=debug");
 await initThreadPool(numConcurrency);
 
 // Config for local development
-// const proof = await prover(
-//   {
-//     notary_host: "localhost",
-//     notary_port: 7443,
-//     target_method: "GET",
-//     target_url: "https://localhost:8085/health",
-//     target_headers: {},
-//     target_body: "",
-//     websocket_proxy_url: "wss://localhost:7443/v1/tlsnotary/websocket_proxy",
-//     notarization_session_request: {
-//       client_type: "Websocket",
-//       max_sent_data: 10000,
-//       max_recv_data: 10000,
-//     }
-//   }
-// );
+const proof = await prover(
+  {
+    notary_host: "localhost",
+    notary_port: 7443,
+    target_method: "GET",
+    target_url: "https://localhost:8085/health",
+    target_headers: {},
+    target_body: "",
+    websocket_proxy_url: "wss://ws.alpha4.tlsnotary.pluto.dev",
+    // websocket_proxy_url: "wss://localhost:7443/v1/tlsnotary/websocket_proxy",
+    notarization_session_request: {
+      client_type: "Websocket",
+      max_sent_data: 10000,
+      max_recv_data: 10000,
+    }
+  }
+);
 
 // Config using notary.pluto.dev
-const proof = await prover({
-  notary_host: "notary.pluto.dev",
-  notary_port: 443,
-  target_method: "GET",
-  target_url: "https://example.com",
-  target_headers: {},
-  target_body: "",
-  websocket_proxy_url: "wss://notary.pluto.dev/v1/tlsnotary/websocket_proxy",
-  notarization_session_request: {
-    client_type: "Websocket",
-    max_sent_data: 10000,
-    max_recv_data: 10000,
-  },
-});
+// const proof = await prover({
+//   notary_host: "notary.pluto.dev",
+//   notary_port: 443,
+//   target_method: "GET",
+//   target_url: "https://example.com",
+//   target_headers: {},
+//   target_body: "",
+//   websocket_proxy_url: "wss://notary.pluto.dev/v1/tlsnotary/websocket_proxy",
+//   notarization_session_request: {
+//     client_type: "Websocket",
+//     max_sent_data: 10000,
+//     max_recv_data: 10000,
+//   },
+// });
 
 console.log(proof);
 
