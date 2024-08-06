31f913ebe60fca000da3dbd6592afb37dd520039
mattes <1240531+mattes@users.noreply.github.com>
2024-08-05 20:05:30 -0700

    refactor client lib a bit

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -6,11 +6,11 @@ use std::collections::HashMap;
 
 use base64::prelude::*;
 use http_body_util::Full;
-use hyper::{body::Bytes, Request};
+use hyper::{body::Bytes, client::conn::http1::SendRequest, Request};
 use serde::{Deserialize, Serialize};
 use tlsn_core::commitment::CommitmentKind;
 pub use tlsn_core::proof::TlsProof;
-use tlsn_prover::tls::{state::ProverState, Prover, ProverConfig};
+use tlsn_prover::tls::{state::Closed, Prover, ProverConfig};
 use tracing::{debug, info, trace};
 use url::Url;
 #[cfg(target_arch = "wasm32")]
@@ -53,52 +53,33 @@ pub enum ClientType {
 }
 
 pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErrors> {
-  info!("client entered `prover_inner` to construct webproof");
-
-  trace!("{config:#?}");
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Parse the target url -> Check that it is HTTPS -> Extract the port being used
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-
-  let _span = tracing::span!(tracing::Level::TRACE, "parse_target_url").entered();
   let target_url = Url::parse(&config.target_url)?;
-
-  trace!("parsed `target_url`: {target_url:?}");
-  // TODO: These three lines with target_url should probably throw a well-defined error instead of
-  // causing panic.
+  info!("target: {}", target_url);
   let target_host = target_url.host_str().expect("Invalid `target_url` host!");
   assert!(target_url.scheme() == "https");
-
-  debug!("parsed `target_host`: {target_host:?}; IS HTTPS!");
-  // Only returns none if no port or known protocol used
   let target_port = target_url.port_or_known_default().expect("Target has an unknown port!");
-  // debug!("parsed `target_port`: {target_port:?}"); // TODO target_port does not exist, fix
-  // feature flags
-
-  info!("target connection data built");
-
-  drop(_span);
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // TODO: The following should be made modular in that we don't want to enforce we are going to
-  // notary approach
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Create a notary session and get back a `NetworkStream` and the session ID
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-
-  let _span = tracing::span!(tracing::Level::TRACE, "connect_to_notary").entered();
-
-  // TODO: (OLD COMMENT?)
-  // Claim back the TLS socket after HTTP exchange is done
-  // #[cfg(not(target_arch = "wasm32"))]
-  // let Parts { io: notary_tls_socket, .. } = connection_task.await??;
-
-  debug!("TLS socket created with TCP connection");
 
   let session_id = uuid::Uuid::new_v4().to_string();
+  info!("session_id: {}", session_id);
+
+  // TODO lot of memory allocation happening here.
+  // maybe add this to shared state?
+  let root_store = default_root_store();
+
+  debug!("Creating prover config");
+  let mut prover_config = ProverConfig::builder();
+  prover_config.id(session_id.clone()).server_dns(target_host).root_cert_store(root_store);
+  prover_config.max_transcript_size(
+    config.notarization_session_request.max_sent_data.unwrap()
+      + config.notarization_session_request.max_recv_data.unwrap(),
+  ); // TODO unwrap
+  let prover_config = prover_config.build()?;
 
   let wss_url = format!(
     "wss://{}:{}/v1/tlsnotary?session_id={}",
-    config.notary_host, config.notary_port, session_id
+    config.notary_host,
+    config.notary_port,
+    session_id.clone()
   );
 
   #[cfg(target_arch = "wasm32")]
@@ -112,25 +93,6 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
     WsStream::new(notary_tls_socket)
   };
 
-  drop(_span);
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Set up the prover which lies on the client and can access the notary for MPC
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-
-  let root_store = default_root_store(); // TODO lot of memory allocation happening here.
-                                         // maybe add this to shared state?
-
-  let _span = tracing::span!(tracing::Level::TRACE, "create_prover").entered();
-  let mut prover_config = ProverConfig::builder();
-  prover_config.id(session_id).server_dns(target_host).root_cert_store(root_store);
-  prover_config.max_transcript_size(
-    config.notarization_session_request.max_sent_data.unwrap()
-      + config.notarization_session_request.max_recv_data.unwrap(),
-  ); // TODO unwrap
-  let prover_config = prover_config.build()?;
-
   // Create a new prover and with MPC backend. use tokio::io::{AsyncReadExt, AsyncWriteExt};
   #[cfg(not(target_arch = "wasm32"))]
   let prover =
@@ -138,20 +100,6 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
   #[cfg(target_arch = "wasm32")]
   let prover = Prover::new(prover_config).setup(notary_tls_socket.into_io()).await?;
 
-  debug!("{prover:?}");
-
-  debug!("prover created");
-
-  drop(_span);
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // TODO: This is where we have to consider using another mode of connection like ws
-  // connect to target
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Connect the client to the target via TLS and maintain it concurrently
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // TODO: This will likely not compile without websocket feature  on wasm since this is a tokio
-  // tcpstream. ALSO, this does noit actually provide a websocket feature for non-wasm.
-  //
   // Bind the Prover to server connection
   #[cfg(any(not(feature = "websocket"), not(target_arch = "wasm32")))]
   let (mpc_tls_connection, prover_fut) = {
@@ -178,7 +126,6 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
   // Grab a control handle to the Prover
   let prover_ctrl = prover_fut.control();
 
-  debug!("prover created");
   // Spawn the Prover to be run concurrently
   #[cfg(not(target_arch = "wasm32"))]
   let prover_task = tokio::spawn(prover_fut);
@@ -195,7 +142,7 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
   };
 
   // Attach the hyper HTTP client to the TLS connection
-  let (mut request_sender, mpc_tls_connection) =
+  let (request_sender, mpc_tls_connection) =
     hyper::client::conn::http1::handshake(mpc_tls_connection).await?;
 
   // Spawn the HTTP task to be run concurrently
@@ -213,74 +160,18 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
     spawn_local(handled_connection_fut);
     connection_receiver
   };
-  //---------------------------------------------------------------------------------------------------------------------------------------//
 
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Build the HTTP request asking the target for some data
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  let mut request = Request::builder().method(config.target_method.as_str()).uri(config.target_url);
-
-  // The following `unwrap()` should be safe since we just created the `Request` above
-  let headers = request.headers_mut().unwrap();
-  // TODO: This could be a source of error as the mapping now just holds a single string, so I will
-  // leave commented out code here.
-  for (key, value) in config.target_headers {
-    //   for (key, values) in config.target_headers {
-    // for value in values {
-    //   headers.append(
-    //     hyper::header::HeaderName::from_bytes(key.as_bytes()).unwrap(),
-    //     value.parse().unwrap(),
-    //   );
-    // }
-    headers.append(hyper::header::HeaderName::from_bytes(key.as_bytes())?, value.parse()?);
-  }
-
-  headers.insert("Host", target_host.parse()?);
-  // Using "identity" instructs the Server not to use compression for its HTTP response.
-  // TLSNotary tooling does not support compression.
-  headers.insert("Accept-Encoding", "identity".parse()?);
-  headers.insert("Connection", "close".parse()?);
-
-  if headers.get("Accept").is_none() {
-    headers.insert("Accept", "*/*".parse()?);
-  }
-
-  let body = if config.target_body.is_empty() {
-    Full::new(Bytes::from(vec![])) // TODO Empty::<Bytes>::new()
-  } else {
-    Full::new(Bytes::from(BASE64_STANDARD.decode(config.target_body)?))
-  };
-
-  let request = request.body(body)?;
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Send the HTTP request from the client to the target
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-
-  debug!("Sending request");
-
-  // Because we don't need to decrypt the response right away, we can defer decryption
-  // until after the connection is closed. This will speed up the proving process!
   prover_ctrl.defer_decryption().await?;
 
-  // TODO: Clean up this logging and error handling
-  match request_sender.send_request(request).await {
-    Ok(response) => {
-      let status = response.status();
-      let _payload = response.into_body();
+  send_request(
+    request_sender,
+    config.target_method,
+    config.target_url,
+    config.target_headers,
+    config.target_body,
+  )
+  .await;
 
-      debug!("Response:\n{:?}", _payload);
-      debug!("Response Status:\n{:?}", status);
-
-      assert!(status.is_success()); // status is 200-299
-
-      debug!("Request OK");
-    },
-    Err(e) if e.is_incomplete_message() => println!("Response: IncompleteMessage (ignored)"), /* TODO */
-    Err(e) => panic!("{:?}", e),
-  };
-
-  debug!("Sent request");
-  //---------------------------------------------------------------------------------------------------------------------------------------//
   #[cfg(target_arch = "wasm32")]
   {
     use futures::AsyncWriteExt;
@@ -288,28 +179,21 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
     client_socket.close().await?;
   }
 
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Complete the prover and notarization
-  //---------------------------------------------------------------------------------------------------------------------------------------//
   let prover = prover_task.await.unwrap()?; // TODO fix unwrap
+  notarize(prover).await
+}
 
-  // Upgrade the prover to an HTTP prover, and start notarization.
+async fn notarize(prover: Prover<Closed>) -> Result<TlsProof, errors::ClientErrors> {
   let mut prover = prover.to_http()?.start_notarize();
 
   // TODO: unwrap for now as we need to bring in `tlsn_formats`
   // Commit to the transcript with the default committer, which will commit using BLAKE3.
   prover.commit().unwrap();
 
-  // Finalize, returning the notarized HTTP session
   let notarized_session = prover.finalize().await?;
 
-  debug!("Notarization complete!");
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // TODO: This is where selective disclosure happens, we should modularize this and verify its
-  // correctness
-  //---------------------------------------------------------------------------------------------------------------------------------------//
-  // Restructure the proof and return it
-  //---------------------------------------------------------------------------------------------------------------------------------------//
+  debug!("Notarization complete");
+
   let session_proof = notarized_session.session_proof();
 
   let mut proof_builder = notarized_session.session().data().build_substrings_proof();
@@ -342,11 +226,87 @@ pub async fn prover_inner(config: Config) -> Result<TlsProof, errors::ClientErro
   //---------------------------------------------------------------------------------------------------------------------------------------//
 }
 
+async fn send_request(
+  mut request_sender: SendRequest<Full<Bytes>>,
+  method: String,
+  url: String,
+  headers: HashMap<String, String>,
+  body: String,
+) {
+  let request = build_request(method, url, headers, body);
+
+  // TODO: Clean up this logging and error handling
+  match request_sender.send_request(request).await {
+    Ok(response) => {
+      let status = response.status();
+      let _payload = response.into_body();
+
+      debug!("Response:\n{:?}", _payload);
+      debug!("Response Status:\n{:?}", status);
+
+      assert!(status.is_success()); // status is 200-299
+
+      debug!("Request OK");
+    },
+    Err(e) if e.is_incomplete_message() => println!("Response: IncompleteMessage (ignored)"), /* TODO */
+    Err(e) => panic!("{:?}", e),
+  };
+}
+
+fn build_request(
+  method: String,
+  url: String,
+  headers: HashMap<String, String>,
+  body: String,
+) -> Request<Full<Bytes>> {
+  let u = Url::parse(&url).unwrap();
+  let target_host = u.host_str().expect("Invalid `target_url` host!");
+  assert!(u.scheme() == "https");
+
+  let mut request = Request::builder().method(method.as_str()).uri(url);
+
+  // The following `unwrap()` should be safe since we just created the `Request` above
+  let h = request.headers_mut().unwrap();
+  // TODO: This could be a source of error as the mapping now just holds a single string, so I will
+  // leave commented out code here.
+  for (key, value) in headers {
+    //   for (key, values) in config.target_headers {
+    // for value in values {
+    //   headers.append(
+    //     hyper::header::HeaderName::from_bytes(key.as_bytes()).unwrap(),
+    //     value.parse().unwrap(),
+    //   );
+    // }
+    h.append(
+      hyper::header::HeaderName::from_bytes(key.as_bytes()).unwrap(),
+      value.parse().unwrap(),
+    );
+  }
+
+  h.insert("Host", target_host.parse().unwrap());
+  // Using "identity" instructs the Server not to use compression for its HTTP response.
+  // TLSNotary tooling does not support compression.
+  h.insert("Accept-Encoding", "identity".parse().unwrap());
+  h.insert("Connection", "close".parse().unwrap());
+
+  if h.get("Accept").is_none() {
+    h.insert("Accept", "*/*".parse().unwrap());
+  }
+
+  let body = if body.is_empty() {
+    Full::new(Bytes::from(vec![])) // TODO Empty::<Bytes>::new()
+  } else {
+    Full::new(Bytes::from(BASE64_STANDARD.decode(body).unwrap()))
+  };
+
+  request.body(body).unwrap()
+}
+
 #[cfg(feature = "notary_ca_cert")]
 const NOTARY_CA_CERT: &[u8] = include_bytes!(env!("NOTARY_CA_CERT_PATH"));
 
 /// Default root store using mozilla certs.
-pub fn default_root_store() -> tls_client::RootCertStore {
+fn default_root_store() -> tls_client::RootCertStore {
   let mut root_store = tls_client::RootCertStore::empty();
   root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
     tls_client::OwnedTrustAnchor::from_subject_spki_name_constraints(
