6b09f63c354e3352429c3fe892d3b4a344e8f082
mattes <1240531+mattes@users.noreply.github.com>
2024-08-23 11:31:31 -0700

    origo connection currently fails because of hyper version mismatch

--- client/src/origo.rs
+++ client/src/origo.rs
@@ -1,36 +1,36 @@
 use std::sync::Arc;
 
+use futures::channel::oneshot;
+use hyper::StatusCode;
 use tlsn_core::proof::TlsProof;
+use tokio_util::compat::TokioAsyncReadCompatExt;
+use tracing::debug;
 
 use crate::{config, errors};
-
-// use tls_client::{ClientConnection, RustCryptoBackend, RustCryptoBackend13, ServerName};
-// use tls_client_async::bind_client;
 
 pub async fn prover_inner_origo(
   mut config: config::Config,
 ) -> Result<TlsProof, errors::ClientErrors> {
   let session_id = config.session_id();
-  let root_store = crate::default_root_store();
+  let root_store = default_root_store();
 
-  let config = tls_client2::ClientConfig::builder()
+  let client_config = tls_client2::ClientConfig::builder()
     .with_safe_defaults()
     .with_root_certificates(root_store)
     .with_no_client_auth();
 
-  //   let origo_conn = OrigoConnection::new();
+  let origo_conn = tls_proxy2::OrigoConnection::new();
   let client = tls_client2::ClientConnection::new(
-    Arc::new(config),
+    Arc::new(client_config),
     Box::new(tls_client2::RustCryptoBackend13::new(origo_conn)),
-    tls_client::ServerName::try_from(target_host).unwrap(),
+    tls_client2::ServerName::try_from(config.target_host().as_str()).unwrap(),
   )
   .unwrap();
-  let (mut client_tls_conn, tls_fut) = tls_client_async2::bind_client(ws_stream.into_io(), client);
 
-  //   let client_notary_config = rustls::ClientConfig::builder()
-  //     .with_safe_defaults()
-  //     .with_root_certificates(root_store)
-  //     .with_no_client_auth();
+  let client_notary_config = rustls::ClientConfig::builder()
+    .with_safe_defaults()
+    .with_root_certificates(crate::prover::default_root_store())
+    .with_no_client_auth();
 
   let notary_connector =
     tokio_rustls::TlsConnector::from(std::sync::Arc::new(client_notary_config));
@@ -51,7 +51,7 @@ pub async fn prover_inner_origo(
 
   let request = hyper::Request::builder()
     .uri(format!(
-      "https://{}:{}/v1/tlsnotary?session_id={}",
+      "https://{}:{}/v1/origo?session_id={}",
       config.notary_host.clone(),
       config.notary_port.clone(),
       session_id.clone(),
@@ -70,9 +70,71 @@ pub async fn prover_inner_origo(
   let hyper::client::conn::Parts { io: notary_tls_socket, .. } =
     connection_task.await.unwrap().unwrap();
 
-  // notary_tls_socket.compat()
+  let (mut client_tls_conn, tls_fut) =
+    tls_client_async2::bind_client(notary_tls_socket.compat(), client);
 
-  // bind_client
+  //   let client_tls_conn = unsafe { FuturesIo::new(client_tls_conn) }; // TODO is this needed?
 
-  todo!("origo mode");
+  // TODO: What do with tls_fut? what do with tls_receiver?
+  let (tls_sender, tls_receiver) = oneshot::channel();
+  let handled_tls_fut = async {
+    let result = tls_fut.await;
+    // Triggered when the server shuts the connection.
+    debug!("tls_sender.send({:?})", result);
+    let _ = tls_sender.send(result);
+  };
+  tokio::spawn(handled_tls_fut);
+
+  let (mut request_sender, connection) =
+    hyper::client::conn::http1::handshake(client_tls_conn).await.unwrap();
+
+  let (connection_sender, connection_receiver) = oneshot::channel();
+  let connection_fut = connection.without_shutdown();
+  let handled_connection_fut = async {
+    let result = connection_fut.await;
+    debug!("connection_sender.send({:?})", result);
+    let _ = connection_sender.send(result);
+  };
+  tokio::spawn(handled_connection_fut);
+
+  let response = request_sender.send_request(config.to_request()).await.unwrap();
+
+  assert_eq!(response.status(), StatusCode::OK);
+
+  let payload = response.into_body().collect().await.unwrap().to_bytes();
+  debug!("Response: {:?}", payload);
+
+  // Close the connection to the server
+  let mut client_socket = connection_receiver.await.unwrap().unwrap().io.into_inner();
+  client_socket.close().await.unwrap();
+
+  todo!("return something");
+}
+
+#[cfg(feature = "notary_ca_cert")]
+const NOTARY_CA_CERT: &[u8] = include_bytes!(env!("NOTARY_CA_CERT_PATH"));
+
+// TODO default_root_store is duplicated in prover.rs because of
+// tls_client::RootCertStore vs rustls::RootCertStore
+
+/// Default root store using mozilla certs.
+fn default_root_store() -> tls_client2::RootCertStore {
+  let mut root_store = tls_client2::RootCertStore::empty();
+  root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+    tls_client2::OwnedTrustAnchor::from_subject_spki_name_constraints(
+      ta.subject.as_ref(),
+      ta.subject_public_key_info.as_ref(),
+      ta.name_constraints.as_ref().map(|nc| nc.as_ref()),
+    )
+  }));
+
+  #[cfg(feature = "notary_ca_cert")]
+  {
+    debug!("notary_ca_cert feature enabled");
+    let certificate = pki_types::CertificateDer::from(NOTARY_CA_CERT.to_vec());
+    let (added, _) = root_store.add_parsable_certificates(&[certificate.to_vec()]); // TODO there is probably a nicer way
+    assert_eq!(added, 1); // TODO there is probably a better way
+  }
+
+  root_store
 }
