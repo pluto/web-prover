2ec3d4d64ee4077e543f0463726feab1b8b10193
mattes <1240531+mattes@users.noreply.github.com>
2024-08-23 08:15:31 -0700

    patch without effect

--- client/src/origo.rs
+++ client/src/origo.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use tlsn_core::proof::TlsProof;
 
 use crate::{config, errors};
@@ -9,12 +11,26 @@ pub async fn prover_inner_origo(
   mut config: config::Config,
 ) -> Result<TlsProof, errors::ClientErrors> {
   let session_id = config.session_id();
-  let root_store = crate::prover::default_root_store();
+  let root_store = crate::default_root_store();
 
-  let client_notary_config = rustls::ClientConfig::builder()
+  let config = tls_client::ClientConfig::builder()
     .with_safe_defaults()
     .with_root_certificates(root_store)
     .with_no_client_auth();
+
+  //   let origo_conn = OrigoConnection::new();
+  let client = tls_client::ClientConnection::new(
+    Arc::new(config),
+    Box::new(tls_client::RustCryptoBackend13::new(origo_conn)),
+    tls_client::ServerName::try_from(target_host).unwrap(),
+  )
+  .unwrap();
+  let (mut client_tls_conn, tls_fut) = tls_client_async::bind_client(ws_stream.into_io(), client);
+
+  //   let client_notary_config = rustls::ClientConfig::builder()
+  //     .with_safe_defaults()
+  //     .with_root_certificates(root_store)
+  //     .with_no_client_auth();
 
   let notary_connector =
     tokio_rustls::TlsConnector::from(std::sync::Arc::new(client_notary_config));
