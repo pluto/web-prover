5ba0355e88622f294a0ae7e6b6dfd6b9488701d9
Colin Roberts <colin@autoparallel.xyz>
2024-08-27 16:11:35 -0600

    include circuits for proofs (#168)
    * init: circomkit
    
    * add in external dependencies
    
    * pin commit in `extractor`
    
    * Create circom_test.yaml
    
    * Update circom_test.yaml
    
    * basic test
    
    * remove useless files

---
+++ proofs/tests/common/index.ts
@@ -0,0 +1,24 @@
+import 'mocha';
+import { Circomkit, WitnessTester } from "circomkit";
+
+export const circomkit = new Circomkit({
+  verbose: false,
+});
+
+export { WitnessTester };
+
+function stringifyValue(value: any): string {
+  if (Array.isArray(value)) {
+    return `[${value.map(stringifyValue).join(', ')}]`;
+  }
+  if (typeof value === 'object' && value !== null) {
+    return `{${Object.entries(value).map(([k, v]) => `${k}: ${stringifyValue(v)}`).join(', ')}}`;
+  }
+  return String(value);
+}
+
+export function generateDescription(input: any): string {
+  return Object.entries(input)
+    .map(([key, value]) => `${key} = ${stringifyValue(value)}`)
+    .join(", ");
+}
