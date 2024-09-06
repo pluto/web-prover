1dd4ecdd115160782cd729ebf6626d06d0f56c82
mattes <1240531+mattes@users.noreply.github.com>
2024-09-06 12:30:50 -0700

    update demo

--- client/src/origo_wasm32.rs
+++ client/src/origo_wasm32.rs
@@ -8,6 +8,7 @@ use std::sync::Arc;
 
 use futures::{channel::oneshot, AsyncWriteExt};
 use hyper::{body::Bytes, Request, StatusCode};
+use serde::Serialize;
 use tls_client2::{ClientConnection, RustCryptoBackend, RustCryptoBackend13, ServerName};
 use tls_client_async2::bind_client;
 use url::Url;
@@ -18,9 +19,8 @@ use crate::{config, errors, OrigoProof, Proof};
 
 pub async fn proxy_and_sign(mut config: config::Config) -> Result<Proof, errors::ClientErrors> {
   let session_id = config.session_id();
-
-  proxy(config, session_id);
-  todo!("proxy_and_sign");
+  let (server_aes_key, server_aes_iv) = proxy(config.clone(), session_id.clone()).await;
+  sign(config.clone(), session_id.clone(), server_aes_key, server_aes_iv).await
 }
 
 async fn proxy(config: config::Config, session_id: String) -> (Vec<u8>, Vec<u8>) {
@@ -87,6 +87,49 @@ async fn proxy(config: config::Config, session_id: String) -> (Vec<u8>, Vec<u8>)
     origo_conn.lock().unwrap().secret_map.get("Handshake:server_aes_key").unwrap().clone();
 
   (server_aes_key, server_aes_iv)
+}
+
+async fn sign(
+  config: config::Config,
+  session_id: String,
+  server_aes_key: Vec<u8>,
+  server_aes_iv: Vec<u8>,
+) -> Result<Proof, errors::ClientErrors> {
+  #[derive(Serialize)]
+  struct SignBody {
+    server_aes_iv:  String,
+    server_aes_key: String,
+  }
+
+  let sb = SignBody {
+    server_aes_iv:  String::from_utf8(server_aes_iv.to_vec()).unwrap(),
+    server_aes_key: String::from_utf8(server_aes_key.to_vec()).unwrap(),
+  };
+
+  let url = format!(
+    "https://{}:{}/v1/origo/sign?session_id={}",
+    config.notary_host.clone(),
+    config.notary_port.clone(),
+    session_id.clone(),
+  );
+
+  let client = reqwest::ClientBuilder::new().build().unwrap();
+
+  #[cfg(feature = "notary_ca_cert")]
+  let client = reqwest::ClientBuilder::new()
+    .add_root_certificate(
+      reqwest::tls::Certificate::from_der(&crate::tls::NOTARY_CA_CERT.to_vec()).unwrap(),
+    )
+    .build()
+    .unwrap();
+
+  let response = client.post(url).json(&sb).send().await.unwrap();
+  assert!(response.status() == hyper::StatusCode::OK);
+
+  // TODO remove debug log line
+  println!("\n{}\n\n", String::from_utf8(response.bytes().await.unwrap().to_vec()).unwrap());
+
+  Ok(Proof::Origo(OrigoProof {})) // TODO
 }
 
 use core::slice;
