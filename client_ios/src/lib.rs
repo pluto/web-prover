f61a31e41a31bb36dc5d58f427fd4ff702a432b3
Colin Roberts <colin@autoparallel.xyz>
2024-09-25 15:46:25 -0600

    add things to client_ios

--- client_ios/src/lib.rs
+++ client_ios/src/lib.rs
@@ -1,6 +1,7 @@
 use std::ffi::{c_char, CStr, CString};
 
 use client::{config::Config, errors::ClientErrors};
+use proofs::{compress::CompressedVerifier, program, ProgramData};
 
 #[derive(serde::Serialize)]
 struct Output {
@@ -51,6 +52,39 @@ pub unsafe extern "C" fn prover(config_json: *const c_char) -> *const c_char {
       };
       let out_json = serde_json::to_string_pretty(&out).unwrap(); // should never panic
       CString::new(out_json).unwrap().into_raw() // should never panic
+    },
+  }
+}
+
+#[no_mangle]
+#[allow(clippy::missing_safety_doc)]
+pub unsafe extern "C" fn run_program_ios(program_data_json: *const c_char) -> *const c_char {
+  let result = std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
+    let program_data_str = unsafe {
+      assert!(!program_data_json.is_null());
+      std::ffi::CStr::from_ptr(program_data_json).to_str().unwrap()
+    };
+    serde_json::from_str::<ProgramData>(program_data_str).unwrap()
+  }));
+
+  match result {
+    Ok(program_data) => {
+      let program_output = program::run(&program_data);
+      let compressed_verifier = CompressedVerifier::from(program_output);
+      let serialized_compressed_verifier = compressed_verifier.serialize_and_compress();
+      std::ffi::CString::new(serialized_compressed_verifier.proof.0).unwrap().into_raw()
+    },
+    Err(err) => {
+      let backtrace = std::backtrace::Backtrace::capture();
+
+      let out = if let Some(e) = err.downcast_ref::<&str>() {
+        format!("Captured Panic\nError: {}\n\nStack:\n{}", e, backtrace)
+      } else {
+        format!("Captured Panic\n{:#?}\n\nStack:\n{}", err, backtrace)
+      };
+
+      let out_json = serde_json::to_string_pretty(&out).unwrap(); // should never panic
+      std::ffi::CString::new(out_json).unwrap().into_raw() // should never panic
     },
   }
 }
