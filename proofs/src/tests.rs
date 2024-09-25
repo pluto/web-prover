8ee71f47c009bc9521794d58f6f4ed4576af354d
Colin Roberts <colin@autoparallel.xyz>
2024-09-25 10:06:29 -0600

    cleanup

--- proofs/src/tests.rs
+++ proofs/src/tests.rs
@@ -1,9 +1,7 @@
 //! This test module is effectively testing a static (comptime) circuit dispatch supernova program
 
-use std::io::{Read, Write};
-
-use arecibo::supernova::snark::CompressedSNARK;
-use flate2::{read::ZlibDecoder, write::ZlibEncoder, Compression};
+use arecibo::supernova::{PublicParams, RecursiveSNARK};
+use compress::{decompress_and_deserialize, serialize_and_compress};
 
 use super::*;
 
@@ -18,9 +16,9 @@ const SQUARE_ZEROTH_GRAPH: &[u8] = include_bytes!("../examples/circuit_data/squa
 const SWAP_MEMORY_R1CS: &str = "examples/circuit_data/swapMemory.r1cs";
 const SWAP_MEMORY_GRAPH: &[u8] = include_bytes!("../examples/circuit_data/swapMemory.bin");
 
-#[test]
-#[tracing_test::traced_test]
-fn test_run() {
+const INIT_PUBLIC_INPUT: [u64; 2] = [1, 2];
+
+fn run_entry() -> (PublicParams<E1>, RecursiveSNARK<E1>) {
   let program_data = ProgramData {
     r1cs_paths:              vec![
       PathBuf::from(ADD_INTO_ZEROTH_R1CS),
@@ -33,12 +31,17 @@ fn test_run() {
       WitnessGeneratorType::Raw(SWAP_MEMORY_GRAPH.to_vec()),
     ],
     rom:                     ROM.to_vec(),
-    initial_public_input:    vec![1, 2],
+    initial_public_input:    INIT_PUBLIC_INPUT.to_vec(),
     private_input:           HashMap::new(),
   };
 
-  let (_pp, recursive_snark) = program::run(&program_data);
+  program::run(&program_data)
+}
 
+#[test]
+#[tracing_test::traced_test]
+fn test_run() {
+  let (_, recursive_snark) = run_entry();
   let final_mem = [
     F::<G1>::from(0),
     F::<G1>::from(81),
@@ -56,49 +59,23 @@ fn test_run() {
 #[test]
 #[tracing_test::traced_test]
 fn test_run_verify() {
-  let mut z0_primary = vec![1, 2];
-  let program_data = ProgramData {
-    r1cs_paths:              vec![
-      PathBuf::from(ADD_INTO_ZEROTH_R1CS),
-      PathBuf::from(SQUARE_ZEROTH_R1CS),
-      PathBuf::from(SWAP_MEMORY_R1CS),
-    ],
-    witness_generator_types: vec![
-      WitnessGeneratorType::Raw(ADD_INTO_ZEROTH_GRAPH.to_vec()),
-      WitnessGeneratorType::Raw(SQUARE_ZEROTH_GRAPH.to_vec()),
-      WitnessGeneratorType::Raw(SWAP_MEMORY_GRAPH.to_vec()),
-    ],
-    rom:                     ROM.to_vec(),
-    initial_public_input:    z0_primary.clone(),
-    private_input:           HashMap::new(),
-  };
-
-  let (public_params, recursive_snark) = program::run(&program_data);
+  let (public_params, recursive_snark) = run_entry();
 
   // Get the CompressedSNARK
   let (_prover_key, verifier_key, compressed_snark) =
     program::compress(&public_params, &recursive_snark);
 
-  // Serialize and compress the proof
-  let bincode = bincode::serialize(&compressed_snark).unwrap();
-  assert_eq!(bincode.len(), 12200);
+  // Serialize and compress further
+  let compressed_proof = serialize_and_compress(&compressed_snark);
 
-  let mut encoder = ZlibEncoder::new(Vec::new(), Compression::best());
-  encoder.write_all(&bincode).unwrap();
-  let compressed = encoder.finish().unwrap();
-  assert_eq!(compressed.len(), 11145);
-
-  // Decompress and Deserialize the proof
-  let mut decoder = ZlibDecoder::new(&compressed[..]);
-  let mut decompressed = Vec::new();
-  decoder.read_to_end(&mut decompressed).unwrap();
-
-  let compressed_snark: CompressedSNARK<E1, S1, S2> = bincode::deserialize(&decompressed).unwrap();
+  // Decompress and deserialize
+  let compressed_snark = decompress_and_deserialize(&compressed_proof);
 
   // Extend the initial state input with the ROM (happens internally inside of `program::run`, so we
   // do it out here)
+  let mut z0_primary = INIT_PUBLIC_INPUT.to_vec();
   z0_primary.push(0);
-  z0_primary.extend(program_data.rom.clone().iter());
+  z0_primary.extend(ROM.iter());
 
   // Check that it verifies
   let res = compressed_snark.verify(
