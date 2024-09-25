0cc985672482424cc2a03bc4d8870b5a2a614a8d
Colin Roberts <colin@autoparallel.xyz>
2024-09-25 13:14:52 -0600

    feature gate internal verification

--- proofs/src/program/mod.rs
+++ proofs/src/program/mod.rs
@@ -165,11 +165,13 @@ pub fn run(program_data: &ProgramData) -> ProgramOutput {
     recursive_snark.prove_step(&public_params, &circuit_primary, &circuit_secondary).unwrap();
     info!("Single step proof took: {:?}", start.elapsed());
 
-    // TODO: I don't think we really need to do this, we can just verify compressed proof
-    info!("Verifying single step...");
-    let start = Instant::now();
-    recursive_snark.verify(&public_params, &z0_primary, &z0_secondary).unwrap();
-    info!("Single step verification took: {:?}", start.elapsed());
+    #[cfg(feature = "verify-steps")]
+    {
+      info!("Verifying single step...");
+      let start = Instant::now();
+      recursive_snark.verify(&public_params, &z0_primary, &z0_secondary).unwrap();
+      info!("Single step verification took: {:?}", start.elapsed());
+    }
 
     // Update everything now for next step
     next_public_input = recursive_snark.zi_primary().clone();
