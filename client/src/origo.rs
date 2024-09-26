c7f1f2800b87d0b453dc3e48bd58d970df60d01c
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-09-26 17:36:05 -0600

    clean up

--- client/src/origo.rs
+++ client/src/origo.rs
@@ -57,27 +57,23 @@ pub async fn sign(
 
   let r = generate_proof(witness).await.unwrap();
 
-  Ok(crate::Proof::Origo()) // TODO
+  Ok(crate::Proof::Origo(r))
 }
 
-pub async fn generate_proof(witness: WitnessData) -> Result<(), errors::ClientErrors> {
+pub async fn generate_proof(witness: WitnessData) -> Result<RecursiveSNARK<Bn256EngineKZG>, errors::ClientErrors> {
 
   debug!("key_as_string: {:?}, length: {}", witness.request.aes_key, witness.request.aes_key.len());
   debug!("iv_as_string: {:?}, length: {}", witness.request.aes_iv, witness.request.aes_iv.len());
 
-  // let mut private_input: HashMap<_, _> = HashMap::new();
   let key: &[u8] = &witness.request.aes_key;
   let iv: &[u8] = &witness.request.aes_iv;
 
   let mut private_input = HashMap::new();
 
-  // can be any length
   let ct: &[u8] = witness.request.ciphertext.as_bytes();
-
   let sized_key: [u8; 16] = key[..16].try_into().unwrap();
   let sized_iv: [u8; 12] = iv[..12].try_into().unwrap();
 
-  /// okay i need to turn this into an array of "numbers"
   private_input.insert("key".to_string(), serde_json::to_value(&sized_key).unwrap());
   private_input.insert("iv".to_string(), serde_json::to_value(&sized_iv).unwrap());
 
@@ -86,7 +82,7 @@ pub async fn generate_proof(witness: WitnessData) -> Result<(), errors::ClientEr
       typ: ContentType::ApplicationData,
       version: ProtocolVersion::TLSv1_3,
       payload:  Payload::new(hex::decode(ct).unwrap())
-  }, 0).unwrap(); // sequence is wrong here
+  }, 0).unwrap();
   let pt = plaintext.payload.0.to_vec();
   let aad = meta.additional_data.as_str().to_owned();
 
@@ -101,31 +97,8 @@ pub async fn generate_proof(witness: WitnessData) -> Result<(), errors::ClientEr
     private_input,
   };
 
-  // private input is the key and iv, and the fold input: plaintext, and aad.
-  // initial public inpute in the example file is a vec of 64 0s.
-
   let (params, proof) = program::run(&program_data);
   debug!("data={:?}", proof);
 
-  Ok(())
-}
-
-fn string_to_fixed_bytes<const N: usize>(s: &str) -> Result<[u8; N], &'static str> {
-  let bytes = s.as_bytes();
-  if bytes.len() != N {
-      return Err("String must be exactly 16 bytes long");
-  }
-  
-  let mut array = [0u8; N];
-  array.copy_from_slice(bytes);
-  Ok(array)
-}
-fn bytes_to_fixed_array<const N: usize>(bytes: &[u8]) -> Result<[u8; N], &'static str> {
-  if bytes.len() != N {
-      return Err("Byte slice length does not match the required size");
-  }
-  
-  let mut array = [0u8; N];
-  array.copy_from_slice(bytes);
-  Ok(array)
+  Ok(proof)
 }
