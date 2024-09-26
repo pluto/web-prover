a5eb08ca0dc84a6bc84aaa8029b5f52e9b51d509
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-09-26 14:22:24 -0600

    made proofs a lib

---
+++ proofs/src/bin/main.rs
@@ -0,0 +1,45 @@
+
+use clap::Parser;
+use proofs::{program, ProgramData};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+use std::path::PathBuf;
+
+
+
+#[derive(Parser, Debug)]
+#[command(name = "prove")]
+pub struct Args {
+  /// Setup file to use for generating proof
+  #[arg(long, short, required = true)]
+  input_file: PathBuf,
+
+  /// Increase logging verbosity (-v, -vv, -vvv, etc.)
+  #[arg(short, long, action = clap::ArgAction::Count)]
+  verbose: u8,
+}
+
+// Note:
+// Run with `cargo run --release -i setup/test.json`
+// from the `./proofs/` dir.
+fn main() {
+  let args = Args::parse();
+
+  // Logging options
+  let log_level = match args.verbose {
+    0 => Level::ERROR,
+    1 => Level::WARN,
+    2 => Level::INFO,
+    3 => Level::DEBUG,
+    _ => Level::TRACE,
+  };
+  let subscriber = FmtSubscriber::builder().with_max_level(log_level).finish();
+  tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+  // Read in the supernova program data
+  let file = args.input_file;
+  info!("Using file: {:?}", file);
+  let read = std::fs::read(file).unwrap();
+  let program_data: ProgramData = serde_json::from_slice(&read).unwrap();
+  program::run(&program_data);
+}
