1dc21dd41d51ce23acf26d4fb8505034fddeb1e0
devloper <3347622+devloper@users.noreply.github.com>
2024-10-14 14:24:07 -0700

    Add demo harness

---
+++ client_ios/demo/demo-app-clip/demo_app_clipApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_app_clipApp.swift
+//  demo-app-clip
+//
+//  Created by devloper on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct demo_app_clipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
