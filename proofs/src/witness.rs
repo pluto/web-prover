b197f22d34f8b7ad4ac98d85ce4153a30d23bacc
Colin Roberts <colin@autoparallel.xyz>
2024-11-08 11:34:26 -0700

    json masking object

--- proofs/src/witness.rs
+++ proofs/src/witness.rs
@@ -69,6 +69,24 @@ mod tests {
     32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 125, 13, 10, 32, 32, 32, 32, 32, 32, 32, 93, 13,
     10, 32, 32, 32, 125, 13, 10, 125,
   ];
+
+  // HTTP/1.1 200 OK
+  // content-type: application/json; charset=utf-8
+  // content-encoding: gzip
+  // Transfer-Encoding: chunked
+  //
+  // {
+  //    "data": {
+  //        "items": [
+  //            {
+  //                "data": "Artist",
+  //                "profile": {
+  //                    "name": "Taylor Swift"
+  //                }
+  //            }
+  //        ]
+  //    }
+  // }
   #[test]
   fn test_compute_json_witness() {
     let json_bytes = TEST_HTTP_BODY
@@ -77,40 +95,82 @@ mod tests {
       .copied()
       .collect::<Vec<u8>>();
     let json: Value = serde_json::from_slice(&json_bytes).unwrap();
-    dbg!(&json);
+    // dbg!(&json);
     let data: &Value = json.get("data").unwrap();
-    dbg!(&data);
+    // dbg!(&data);
     let data_bytes = serde_json::to_string(&data).unwrap();
     let data_bytes = data_bytes.as_bytes();
-    dbg!(data_bytes);
 
     let filtered_data_bytes = data_bytes
       .iter()
       .filter(|&&x| !x == 0 || !x.is_ascii_whitespace())
       .copied()
       .collect::<Vec<u8>>();
+    dbg!(&filtered_data_bytes);
     let mut start_idx: Option<usize> = None;
+    let mut end_idx: Option<usize> = None;
     // Iterate through the original bytes and find the subobject
-    for idx in 0..TEST_HTTP_BODY.len() - filtered_data_bytes.len() {
+    for (idx, byte) in TEST_HTTP_BODY.iter().enumerate() {
       // If we find that the byte we are at matches the first byte of our subobject, then we should
       // see if this string matches
-      if idx == 132 {
-        dbg!(TEST_HTTP_BODY[idx..]
-          .iter()
-          .filter(|&&x| !x == 0 || !x.is_ascii_whitespace())
-          .copied()
-          .collect::<Vec<u8>>());
-      }
-      if filtered_data_bytes
-        == TEST_HTTP_BODY[idx..(idx + filtered_data_bytes.len())]
-          .iter()
-          .filter(|&&x| !x == 0 || !x.is_ascii_whitespace())
-          .copied()
-          .collect::<Vec<u8>>()
-      {
+      let mut filtered_body = TEST_HTTP_BODY[idx..]
+        .iter()
+        .filter(|&&x| !x == 0 || !x.is_ascii_whitespace())
+        .copied()
+        .collect::<Vec<u8>>();
+      filtered_body.truncate(filtered_data_bytes.len());
+      //   if idx == 132 {
+      //     dbg!(&filtered_body);
+      //   }
+      if filtered_data_bytes == filtered_body && filtered_data_bytes.first().unwrap() == byte {
         start_idx = Some(idx);
       }
     }
+    for (idx, byte) in TEST_HTTP_BODY.iter().enumerate() {
+      let mut filtered_body = TEST_HTTP_BODY[..idx + 1]
+        .iter()
+        .filter(|&&x| !x == 0 || !x.is_ascii_whitespace())
+        .copied()
+        .collect::<Vec<u8>>();
+      filtered_body.reverse();
+      filtered_body.truncate(filtered_data_bytes.len());
+      filtered_body.reverse();
+      if idx == 319 {
+        dbg!(&filtered_body);
+        dbg!(filtered_data_bytes.last());
+        dbg!(byte);
+      }
+
+      if filtered_data_bytes == filtered_body && filtered_data_bytes.last().unwrap() == byte {
+        end_idx = Some(idx);
+      }
+    }
     dbg!(start_idx);
+    dbg!(end_idx);
+    let start_idx = start_idx.unwrap();
+    let end_idx = end_idx.unwrap();
+    let masked_array = TEST_HTTP_BODY
+      .iter()
+      .enumerate()
+      .map(|(i, &x)| if i >= start_idx && i <= end_idx { x } else { 0 })
+      .collect::<Vec<u8>>();
+    // dbg!(&TEST_HTTP_BODY[start_idx..end_idx]);
+    dbg!(masked_array);
   }
 }
+
+const MASKED_ARRAY: [u8; 320] = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 123, 13, 10, 32, 32, 32, 32, 32, 32, 32, 34, 105, 116, 101, 109, 115, 34, 58, 32, 91,
+  13, 10, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 123, 13, 10, 32, 32, 32, 32, 32, 32, 32, 32,
+  32, 32, 32, 32, 32, 32, 32, 34, 100, 97, 116, 97, 34, 58, 32, 34, 65, 114, 116, 105, 115, 116,
+  34, 44, 13, 10, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 34, 112, 114, 111,
+  102, 105, 108, 101, 34, 58, 32, 123, 13, 10, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+  32, 32, 32, 34, 110, 97, 109, 101, 34, 58, 32, 34, 84, 97, 121, 108, 111, 114, 32, 83, 119, 105,
+  102, 116, 34, 13, 10, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 125, 13, 10,
+  32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 125, 13, 10, 32, 32, 32, 32, 32, 32, 32, 93, 13, 10,
+  32, 32, 32, 125, 0, 0, 0,
+];
