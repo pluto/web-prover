f24016910cf6342f3884d5a81bb7d50ee44753e4
Colin Roberts <colin@autoparallel.xyz>
2024-11-13 14:24:37 -0700

    fix: `client` crate build/check

--- client/src/circuits.rs
+++ client/src/circuits.rs
@@ -9,10 +9,10 @@ pub const AES_GCM_GRAPH: &[u8] =
   include_bytes!("../../proofs/web_proof_circuits/target_512b/aes_gctr_nivc_512b.bin");
 
 // Circuit 1
-const HTTP_NIVC_R1CS: &[u8] =
-  include_bytes!("../../web_proof_circuits/target_512b/http_nivc_512b.r1cs");
-const HTTP_NIVC_GRAPH: &[u8] =
-  include_bytes!("../../web_proof_circuits/target_512b/http_nivc_512b.bin");
+pub const HTTP_NIVC_R1CS: &[u8] =
+  include_bytes!("../../proofs/web_proof_circuits/target_512b/http_nivc_512b.r1cs");
+pub const HTTP_NIVC_GRAPH: &[u8] =
+  include_bytes!("../../proofs/web_proof_circuits/target_512b/http_nivc_512b.bin");
 
 // Circuit 1
 pub const HTTP_PARSE_AND_LOCK_START_LINE_R1CS: &[u8] = include_bytes!(
@@ -52,7 +52,7 @@ pub const EXTRACT_VALUE_R1CS: &[u8] =
 pub const EXTRACT_VALUE_GRAPH: &[u8] =
   include_bytes!("../../proofs/web_proof_circuits/target_512b/json_extract_value_512b.bin");
 
-const AES_COUNTER: (&str, [u8; 128]) = ("ctr", [
+pub const AES_COUNTER: (&str, [u8; 128]) = ("ctr", [
   0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 5, 0, 0, 0, 6, 0, 0, 0, 7, 0, 0, 0, 8,
   0, 0, 0, 9, 0, 0, 0, 10, 0, 0, 0, 11, 0, 0, 0, 12, 0, 0, 0, 13, 0, 0, 0, 14, 0, 0, 0, 15, 0, 0,
   0, 16, 0, 0, 0, 17, 0, 0, 0, 18, 0, 0, 0, 19, 0, 0, 0, 20, 0, 0, 0, 21, 0, 0, 0, 22, 0, 0, 0, 23,
