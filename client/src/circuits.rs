b9dd7ca9aea3e64c0584630eeb33ddd3f317f600
lonerapier <lonerapier@proton.me>
2024-11-14 01:26:09 +0530

    wip: save

--- client/src/circuits.rs
+++ client/src/circuits.rs
@@ -9,6 +9,12 @@ pub const AES_GCM_GRAPH: &[u8] =
   include_bytes!("../../proofs/web_proof_circuits/target_512b/aes_gctr_nivc_512b.bin");
 
 // Circuit 1
+const HTTP_NIVC_R1CS: &[u8] =
+  include_bytes!("../../web_proof_circuits/target_512b/http_nivc_512b.r1cs");
+const HTTP_NIVC_GRAPH: &[u8] =
+  include_bytes!("../../web_proof_circuits/target_512b/http_nivc_512b.bin");
+
+// Circuit 1
 pub const HTTP_PARSE_AND_LOCK_START_LINE_R1CS: &[u8] = include_bytes!(
   "../../proofs/web_proof_circuits/target_512b/http_parse_and_lock_start_line_512b.r1cs"
 );
@@ -45,3 +51,11 @@ pub const EXTRACT_VALUE_R1CS: &[u8] =
   include_bytes!("../../proofs/web_proof_circuits/target_512b/json_extract_value_512b.r1cs");
 pub const EXTRACT_VALUE_GRAPH: &[u8] =
   include_bytes!("../../proofs/web_proof_circuits/target_512b/json_extract_value_512b.bin");
+
+const AES_COUNTER: (&str, [u8; 128]) = ("ctr", [
+  0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 5, 0, 0, 0, 6, 0, 0, 0, 7, 0, 0, 0, 8,
+  0, 0, 0, 9, 0, 0, 0, 10, 0, 0, 0, 11, 0, 0, 0, 12, 0, 0, 0, 13, 0, 0, 0, 14, 0, 0, 0, 15, 0, 0,
+  0, 16, 0, 0, 0, 17, 0, 0, 0, 18, 0, 0, 0, 19, 0, 0, 0, 20, 0, 0, 0, 21, 0, 0, 0, 22, 0, 0, 0, 23,
+  0, 0, 0, 24, 0, 0, 0, 25, 0, 0, 0, 26, 0, 0, 0, 27, 0, 0, 0, 28, 0, 0, 0, 29, 0, 0, 0, 30, 0, 0,
+  0, 31, 0, 0, 0, 32,
+]);
