be46e0bccdb33ac55b7a38a913844d94aa350db5
mattes <1240531+mattes@users.noreply.github.com>
2024-11-13 17:28:02 -0800

    add more client code

--- client/src/tee_native.rs
+++ client/src/tee_native.rs
@@ -1,5 +1,125 @@
 use crate::{config, errors, Proof};
 
 pub async fn proxy_and_attest(mut config: config::Config) -> Result<Proof, errors::ClientErrors> {
+  // call new_session
+  // proxy
+  // get attestation
+
   todo!()
+}
+
+async fn proxy(
+  config: config::Config,
+  session_id: String,
+) -> Result<(SignBody, WitnessData), ClientErrors> {
+  let root_store = crate::tls::tls_client2_default_root_store();
+
+  let client_config = tls_client2::ClientConfig::builder()
+    .with_safe_defaults()
+    .with_root_certificates(root_store)
+    .with_no_client_auth();
+
+  let origo_conn = Arc::new(std::sync::Mutex::new(OrigoConnection::new()));
+  let client = tls_client2::ClientConnection::new(
+    Arc::new(client_config),
+    Box::new(tls_client2::RustCryptoBackend13::new(origo_conn.clone())),
+    tls_client2::ServerName::try_from(config.target_host()?.as_str()).unwrap(),
+  )?;
+
+  let client_notary_config = rustls::ClientConfig::builder()
+    .with_safe_defaults()
+    .with_root_certificates(crate::tls::rustls_default_root_store())
+    .with_no_client_auth();
+
+  let notary_connector =
+    tokio_rustls::TlsConnector::from(std::sync::Arc::new(client_notary_config));
+
+  let notary_socket =
+    tokio::net::TcpStream::connect((config.notary_host.clone(), config.notary_port.clone()))
+      .await?;
+
+  let notary_tls_socket = notary_connector
+    .connect(rustls::ServerName::try_from(config.notary_host.as_str())?, notary_socket)
+    .await?;
+
+  let notary_tls_socket = hyper_util::rt::TokioIo::new(notary_tls_socket);
+
+  let (mut request_sender, connection) =
+    hyper::client::conn::http1::handshake(notary_tls_socket).await?;
+  let connection_task = tokio::spawn(connection.without_shutdown());
+
+  // TODO build sanitized query
+  let request: Request<Full<Bytes>> = hyper::Request::builder()
+    .uri(format!(
+      "https://{}:{}/v1/origo?session_id={}&target_host={}&target_port={}",
+      config.notary_host.clone(),
+      config.notary_port.clone(),
+      session_id.clone(),
+      config.target_host()?,
+      config.target_port()?,
+    ))
+    .method("GET")
+    .header("Host", config.notary_host.clone())
+    .header("Connection", "Upgrade")
+    .header("Upgrade", "TCP")
+    .body(http_body_util::Full::default())
+    .unwrap();
+
+  let response = request_sender.send_request(request).await?;
+  assert!(response.status() == hyper::StatusCode::SWITCHING_PROTOCOLS);
+
+  // Claim back the TLS socket after the HTTP to TCP upgrade is done
+  let hyper::client::conn::http1::Parts { io: notary_tls_socket, .. } = connection_task.await??;
+
+  // TODO notary_tls_socket needs to implement futures::AsyncRead/Write, find a better wrapper here
+  let notary_tls_socket = hyper_util::rt::TokioIo::new(notary_tls_socket);
+
+  let (client_tls_conn, tls_fut) =
+    tls_client_async2::bind_client(notary_tls_socket.compat(), client);
+
+  // TODO: What do with tls_fut? what do with tls_receiver?
+  let (tls_sender, _tls_receiver) = oneshot::channel();
+  let handled_tls_fut = async {
+    let result = tls_fut.await.unwrap();
+    let _ = tls_sender.send(result);
+  };
+  tokio::spawn(handled_tls_fut);
+
+  let client_tls_conn = hyper_util::rt::TokioIo::new(client_tls_conn.compat());
+
+  let (mut request_sender, connection) =
+    hyper::client::conn::http1::handshake(client_tls_conn).await?;
+
+  let (connection_sender, connection_receiver) = oneshot::channel();
+  let connection_fut = connection.without_shutdown();
+  let handled_connection_fut = async {
+    let result = connection_fut.await;
+    let _ = connection_sender.send(result);
+  };
+  tokio::spawn(handled_connection_fut);
+
+  let response = request_sender.send_request(config.to_request()?).await?;
+
+  assert_eq!(response.status(), StatusCode::OK);
+
+  let payload = response.into_body().collect().await?.to_bytes();
+  debug!("Response: {:?}", payload);
+
+  // Close the connection to the server
+  // TODO this closes the TLS Connection, do we want to maybe close the TCP stream instead?
+  let mut client_socket = connection_receiver.await??.io.into_inner().into_inner();
+  client_socket.close().await?;
+
+  let server_aes_iv =
+    origo_conn.lock().unwrap().secret_map.get("Handshake:server_aes_iv").unwrap().clone();
+  let server_aes_key =
+    origo_conn.lock().unwrap().secret_map.get("Handshake:server_aes_key").unwrap().clone();
+
+  let witness = origo_conn.lock().unwrap().to_witness_data();
+  let sb = SignBody {
+    hs_server_aes_iv:  hex::encode(server_aes_iv.to_vec()),
+    hs_server_aes_key: hex::encode(server_aes_key.to_vec()),
+  };
+
+  Ok((sb, witness))
 }
--- client/src/tee_wasm32.rs
+++ client/src/tee_wasm32.rs
@@ -1,5 +1,77 @@
 use crate::{config, errors, Proof};
 
-pub async fn proxy_and_attest(mut config: config::Config) -> Result<Proof, errors::ClientErrors> {
-  todo!()
+pub async fn proxy_and_attest(mut config: config::Config) -> Result<Proof, errors::ClientErrors> {}
+
+async fn proxy(
+  config: config::Config,
+  session_id: String,
+) -> Result<(SignBody, WitnessData), errors::ClientErrors> {
+  // TODO build sanitized query
+  let wss_url = format!(
+    "wss://{}:{}/v1/origo?session_id={}&target_host={}&target_port={}",
+    config.notary_host,
+    config.notary_port,
+    session_id.clone(),
+    config.target_host()?,
+    config.target_port()?,
+  );
+
+  let root_store = crate::tls::tls_client2_default_root_store();
+
+  let client_config = tls_client2::ClientConfig::builder()
+    .with_safe_defaults()
+    .with_root_certificates(root_store)
+    .with_no_client_auth();
+
+  let origo_conn = Arc::new(std::sync::Mutex::new(tls_client2::origo::OrigoConnection::new()));
+  let client = tls_client2::ClientConnection::new(
+    Arc::new(client_config),
+    Box::new(tls_client2::RustCryptoBackend13::new(origo_conn.clone())),
+    tls_client2::ServerName::try_from(config.target_host()?.as_str())?,
+  )?;
+
+  let (_, ws_stream) = WsMeta::connect(wss_url.to_string(), None).await?;
+
+  let (mut client_tls_conn, tls_fut) = bind_client(ws_stream.into_io(), client);
+
+  let client_tls_conn = unsafe { FuturesIo::new(client_tls_conn) };
+
+  let (tls_sender, tls_receiver) = oneshot::channel();
+  let handled_tls_fut = async {
+    let result = tls_fut.await;
+    // Triggered when the server shuts the connection.
+    let _ = tls_sender.send(result);
+  };
+  spawn_local(handled_tls_fut);
+
+  let (mut request_sender, connection) =
+    hyper::client::conn::http1::handshake(client_tls_conn).await?;
+
+  let (connection_sender, connection_receiver) = oneshot::channel();
+  let connection_fut = connection.without_shutdown();
+  let handled_connection_fut = async {
+    let result = connection_fut.await;
+    let _ = connection_sender.send(result);
+  };
+  spawn_local(handled_connection_fut);
+
+  let response = request_sender.send_request(config.to_request()?).await?;
+
+  assert_eq!(response.status(), StatusCode::OK);
+
+  let mut client_socket = connection_receiver.await.unwrap()?.io.into_inner();
+  client_socket.close().await.unwrap();
+
+  let server_aes_iv =
+    origo_conn.lock().unwrap().secret_map.get("Handshake:server_aes_iv").unwrap().clone();
+  let server_aes_key =
+    origo_conn.lock().unwrap().secret_map.get("Handshake:server_aes_key").unwrap().clone();
+
+  let witness = origo_conn.lock().unwrap().to_witness_data();
+  let sb = SignBody {
+    hs_server_aes_iv:  hex::encode(server_aes_iv.to_vec()),
+    hs_server_aes_key: hex::encode(server_aes_key.to_vec()),
+  };
+
+  Ok((sb, witness))
 }
