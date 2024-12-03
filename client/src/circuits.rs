129b1c73ce5137b633de2f208388733082b3d97d
lonerapier <lonerapier@proton.me>
2024-12-04 00:35:04 +0530

    fix circuits for wasm

--- client/src/circuits.rs
+++ client/src/circuits.rs
@@ -83,7 +83,6 @@ pub const EXTRACT_VALUE_1024_R1CS: &[u8] =
 pub const EXTRACT_VALUE_1024_GRAPH: &[u8] =
   include_bytes!("../../proofs/web_proof_circuits/target_1024b/json_extract_value_1024b.bin");
 
-#[cfg(not(target_arch = "wasm32"))]
 /// construct [`SetupData`] with all the required circuits for 512B inputs
 pub fn construct_setup_data_512(cipher_suite_key: &tls_client2::CipherSuiteKey) -> SetupData {
   let (encryption_circuit_r1cs, encryption_circuit_graph) = match cipher_suite_key {
@@ -91,13 +90,14 @@ pub fn construct_setup_data_512(cipher_suite_key: &tls_client2::CipherSuiteKey) 
     tls_client2::CipherSuiteKey::CHACHA20POLY1305(_) => (CHACHA20_R1CS, CHACHA20_GRAPH),
   };
   SetupData {
-    r1cs_types:              vec![
+    r1cs_types: vec![
       R1CSType::Raw(encryption_circuit_r1cs.to_vec()),
       R1CSType::Raw(HTTP_NIVC_R1CS.to_vec()),
       R1CSType::Raw(JSON_MASK_OBJECT_R1CS.to_vec()),
       R1CSType::Raw(JSON_MASK_ARRAY_INDEX_R1CS.to_vec()),
       R1CSType::Raw(EXTRACT_VALUE_R1CS.to_vec()),
     ],
+    #[cfg(not(target_arch = "wasm32"))]
     witness_generator_types: vec![
       WitnessGeneratorType::Raw(encryption_circuit_graph.to_vec()),
       WitnessGeneratorType::Raw(HTTP_NIVC_GRAPH.to_vec()),
@@ -105,11 +105,18 @@ pub fn construct_setup_data_512(cipher_suite_key: &tls_client2::CipherSuiteKey) 
       WitnessGeneratorType::Raw(JSON_MASK_ARRAY_INDEX_GRAPH.to_vec()),
       WitnessGeneratorType::Raw(EXTRACT_VALUE_GRAPH.to_vec()),
     ],
-    max_rom_length:          JSON_MAX_ROM_LENGTH,
+    #[cfg(target_arch = "wasm32")]
+    witness_generator_types: vec![
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+    ],
+    max_rom_length: JSON_MAX_ROM_LENGTH,
   }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 /// construct [`SetupData`] with all the required inputs for 1024B inputs
 pub fn construct_setup_data_1024(cipher_suite_key: &tls_client2::CipherSuiteKey) -> SetupData {
   let (encryption_circuit_r1cs, encryption_circuit_graph) = match cipher_suite_key {
@@ -117,13 +124,14 @@ pub fn construct_setup_data_1024(cipher_suite_key: &tls_client2::CipherSuiteKey)
     tls_client2::CipherSuiteKey::CHACHA20POLY1305(_) => (CHACHA20_1024_R1CS, CHACHA20_1024_GRAPH),
   };
   SetupData {
-    r1cs_types:              vec![
+    r1cs_types: vec![
       R1CSType::Raw(encryption_circuit_r1cs.to_vec()),
       R1CSType::Raw(HTTP_NIVC_1024_R1CS.to_vec()),
       R1CSType::Raw(JSON_MASK_OBJECT_1024_R1CS.to_vec()),
       R1CSType::Raw(JSON_MASK_ARRAY_INDEX_1024_R1CS.to_vec()),
       R1CSType::Raw(EXTRACT_VALUE_1024_R1CS.to_vec()),
     ],
+    #[cfg(not(target_arch = "wasm32"))]
     witness_generator_types: vec![
       WitnessGeneratorType::Raw(encryption_circuit_graph.to_vec()),
       WitnessGeneratorType::Raw(HTTP_NIVC_1024_GRAPH.to_vec()),
@@ -131,7 +139,15 @@ pub fn construct_setup_data_1024(cipher_suite_key: &tls_client2::CipherSuiteKey)
       WitnessGeneratorType::Raw(JSON_MASK_ARRAY_INDEX_1024_GRAPH.to_vec()),
       WitnessGeneratorType::Raw(EXTRACT_VALUE_1024_GRAPH.to_vec()),
     ],
-    max_rom_length:          JSON_MAX_ROM_1024B_LENGTH,
+    #[cfg(target_arch = "wasm32")]
+    witness_generator_types: vec![
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+      WitnessGeneratorType::Browser,
+    ],
+    max_rom_length: JSON_MAX_ROM_1024B_LENGTH,
   }
 }
 
@@ -143,49 +159,5 @@ pub fn construct_setup_data(
     512 => construct_setup_data_512(&cipher_suite_key),
     1024 => construct_setup_data_1024(&cipher_suite_key),
     _ => panic!("not supported plaintext length > 1KB"),
-  }
-}
-
-#[cfg(target_arch = "wasm32")]
-/// construct [`SetupData`] with all the required inputs for 1024B inputs
-pub fn construct_setup_data_512() -> SetupData {
-  SetupData {
-    r1cs_types:              vec![
-      R1CSType::Raw(AES_GCM_R1CS.to_vec()),
-      R1CSType::Raw(HTTP_NIVC_R1CS.to_vec()),
-      R1CSType::Raw(JSON_MASK_OBJECT_R1CS.to_vec()),
-      R1CSType::Raw(JSON_MASK_ARRAY_INDEX_R1CS.to_vec()),
-      R1CSType::Raw(EXTRACT_VALUE_R1CS.to_vec()),
-    ],
-    witness_generator_types: vec![
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-    ],
-    max_rom_length:          JSON_MAX_ROM_LENGTH,
-  }
-}
-
-#[cfg(target_arch = "wasm32")]
-/// construct [`SetupData`] with all the required circuits for 512B inputs
-pub fn construct_setup_data_1024() -> SetupData {
-  SetupData {
-    r1cs_types:              vec![
-      R1CSType::Raw(AES_GCM_1024_R1CS.to_vec()),
-      R1CSType::Raw(HTTP_NIVC_1024_R1CS.to_vec()),
-      R1CSType::Raw(JSON_MASK_OBJECT_1024_R1CS.to_vec()),
-      R1CSType::Raw(JSON_MASK_ARRAY_INDEX_1024_R1CS.to_vec()),
-      R1CSType::Raw(EXTRACT_VALUE_1024_R1CS.to_vec()),
-    ],
-    witness_generator_types: vec![
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-      WitnessGeneratorType::Browser,
-    ],
-    max_rom_length:          JSON_MAX_ROM_1024B_LENGTH,
   }
 }
