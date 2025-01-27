9885985facc880611d2164422b63328db3971d28
Waylon Jepsen <waylonjepsen1@gmail.com>
2025-01-27 08:59:08 -0700

    wip: compiling test,  TODO(WJ 2025-01-27): Certificate errors

--- client_ios/demo/test/proverTest.swift
+++ client_ios/demo/test/proverTest.swift
@@ -18,90 +18,103 @@ final class ProverTests: XCTestCase {
     let localUrl = "https://gist.githubusercontent.com/mattes/23e64faadb5fd4b5112f379903d2572e/raw/74e517a60c21a5c11d94fec8b572f68addfade39/example.json"
     let circuit = "plaintext_authentication_512b.r1cs"
     
-    var r1cs_url: String
-    override init() {
+    var r1cs_url: String = ""
+    override func setUp() {
+        super.setUp()
         r1cs_url = "https://localhost:8090/build/circom-artifacts-512b-v0.7.3/\(circuit)"
     }
     
     // TODO: Live app should fetch these in parallel
-    fetchData(from: r1cs_url) { (r1cs_data, error) in
-        if let error = error {
-            print("Error: \(error.localizedDescription)")
-            return
-        }
-        
-        if let data = r1cs_data {
+    func testFetchAndProve() {
+        let expectation = XCTestExpectation(description: "Fetch and prove data")
+                
+            // Move the fetch code inside the test method
+        fetchData(from: r1cs_url) { (r1cs_data, error) in
+            if let error = error {
+                XCTFail("Failed to fetch data: \(error.localizedDescription)")
+                expectation.fulfill()
+                return
+            }
+            
+            guard let data = r1cs_data else {
+                XCTFail("No data received")
+                expectation.fulfill()
+                return
+            }
+            
+            
             print("data: \(data)")
             let arrayString = data.map { String($0) }.joined(separator: ",")
-            
             let jsonString = """
-             {
-                 "max_recv_data": 10000,
-                 "max_sent_data": 10000,
-                 "mode": "Origo",
-                 "notary_host": "\(localHost)",
-                 "notary_port": \(localPort),
-                 "target_body": "",
-                 "target_headers": {
-                     \(localAuthHeader)
-                     "Content-Type": "application/json",
-                     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
-                 },
-                 "target_method": "\(localMethod)",
-                 "target_url": "\(localUrl)",
-                 "proving": {
-                     "manifest": {
-                         "manifestVersion": "1",
-                         "id": "reddit-user-karma",
-                         "title": "Total Reddit Karma",
-                         "description": "Generate a proof that you have a certain amount of karma",
-                         "prepareUrl": "https://www.reddit.com/login/",
-                         "request": {
-                             "method": "GET",
-                             "version": "HTTP/1.1",
-                             "url": "https://gist.githubusercontent.com/mattes/23e64faadb5fd4b5112f379903d2572e/raw/74e517a60c21a5c11d94fec8b572f68addfade39/example.json",
-                             "headers": {
-                                 "accept-encoding": "identity"
-                             },
-                             "body": {
-                                 "userId": "<% userId %>"
-                             },
-                             "vars": {
-                                 "userId": {
-                                     "regex": "[a-z]{,20}+"
+                 {
+                     "max_recv_data": 10000,
+                     "max_sent_data": 10000,
+                     "mode": "Origo",
+                     "notary_host": "\(self.localHost)",
+                     "notary_port": \(self.localPort),
+                     "target_body": "",
+                     "target_headers": {
+                         \(self.localAuthHeader)
+                         "Content-Type": "application/json",
+                         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+                     },
+                     "target_method": "\(self.localMethod)",
+                     "target_url": "\(self.localUrl)",
+                     "proving": {
+                         "manifest": {
+                             "manifestVersion": "1",
+                             "id": "reddit-user-karma",
+                             "title": "Total Reddit Karma",
+                             "description": "Generate a proof that you have a certain amount of karma",
+                             "prepareUrl": "https://www.reddit.com/login/",
+                             "request": {
+                                 "method": "GET",
+                                 "version": "HTTP/1.1",
+                                 "url": "https://gist.githubusercontent.com/mattes/23e64faadb5fd4b5112f379903d2572e/raw/74e517a60c21a5c11d94fec8b572f68addfade39/example.json",
+                                 "headers": {
+                                     "accept-encoding": "identity"
                                  },
-                                 "token": {
-                                     "type": "base64",
-                                     "length": 32
+                                 "body": {
+                                     "userId": "<% userId %>"
+                                 },
+                                 "vars": {
+                                     "userId": {
+                                         "regex": "[a-z]{,20}+"
+                                     },
+                                     "token": {
+                                         "type": "base64",
+                                         "length": 32
+                                     }
                                  }
-                             }
-                         },
-                         "response": {
-                             "status": "200",
-                             "version": "HTTP/1.1",
-                             "message": "OK",
-                             "headers": {
-                                 "Content-Type": "text/plain"
                              },
-                             "body": {
-                                 "json": [
-                                     "hello"
-                                 ],
-                                 "contains": "this_string_exists_in_body"
+                             "response": {
+                                 "status": "200",
+                                 "version": "HTTP/1.1",
+                                 "message": "OK",
+                                 "headers": {
+                                     "Content-Type": "text/plain"
+                                 },
+                                 "body": {
+                                     "json": [
+                                         "hello"
+                                     ],
+                                     "contains": "this_string_exists_in_body"
+                                 }
                              }
                          }
                      }
                  }
-             }
-             """
-            
+            """
             // NOTE: Witness generation happen in the library for ios
             jsonString.withCString { (cString) in
                 Prover.prover(cString)
             }
-            let timeElapsed = CFAbsoluteTimeGetCurrent() - start
+            let timeElapsed = CFAbsoluteTimeGetCurrent() - self.start
             print("Time elapsed: \(timeElapsed) seconds")
+            expectation.fulfill()
         }
+        wait(for: [expectation], timeout: 30.0)
+        
     }
     
 }
