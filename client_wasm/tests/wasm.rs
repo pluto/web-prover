ac249f933ec23cccc3d57a8b0a2d8aa5ebffd6ab
mattes <1240531+mattes@users.noreply.github.com>
2025-01-28 15:49:34 -0800

    add test

--- client_wasm/tests/wasm.rs
+++ client_wasm/tests/wasm.rs
@@ -1,14 +1,26 @@
-use client::{config, prover_inner};
+use client::{
+  config::{self, NotaryMode},
+  prover_inner,
+};
 use wasm_bindgen_test::*;
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn test_prover_inner() {
-  // TODO
-  //   let config: config::Config = {
-  // 	mode: Origo,,
-  //   };
-  //   let proving_params = todo!();
-  //   prover_inner(config, proving_params);
-  assert_eq!(1, 1)
+async fn test_prover_inner() {
+  let config_json = std::fs::read_to_string("../fixture/client.origo_tcp_local.json").unwrap();
+  let mut config: config::Config = serde_json::from_str(&config_json).unwrap();
+  config.set_session_id();
+
+  let proving_params = std::fs::read(circuits::PROVING_PARAMS_1024).unwrap();
+
+  let proof = prover_inner(config, Some(proving_params)).await.unwrap();
+
+  match proof {
+    client::Proof::TLSN(tls_proof) => panic!("expect Origo proof"),
+    client::Proof::Origo(origo_proof) => {
+      // TODO
+      assert_eq!(1, 1)
+    },
+    client::Proof::TEE() => panic!("expect Origo proof"),
+  }
 }
