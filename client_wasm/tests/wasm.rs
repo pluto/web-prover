d2c32603a1ca7966c7dc2a794c29048e888c416f
mattes <1240531+mattes@users.noreply.github.com>
2025-01-28 15:33:32 -0800

    add todos

--- client_wasm/tests/wasm.rs
+++ client_wasm/tests/wasm.rs
@@ -1,7 +1,14 @@
+use client::{config, prover_inner};
 use wasm_bindgen_test::*;
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-  assert_eq!(1, 1);
+fn test_prover_inner() {
+  // TODO
+  //   let config: config::Config = {
+  // 	mode: Origo,,
+  //   };
+  //   let proving_params = todo!();
+  //   prover_inner(config, proving_params);
+  assert_eq!(1, 1)
 }
