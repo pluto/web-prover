f326eabec63312d92a0fdb249eda72c3fde064b7
mattes <1240531+mattes@users.noreply.github.com>
2025-01-28 17:03:12 -0800

    hm

--- client_wasm/tests/wasm.rs
+++ client_wasm/tests/wasm.rs
@@ -8,13 +8,13 @@ wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn test_prover_inner() {
-  let config_json = include_bytes!("../fixture/client.origo_tcp_local.json");
-  let mut config: config::Config = serde_json::from_str(&config_json).unwrap();
+  let config_json = include_bytes!("../../fixture/client.origo_tcp_local.json");
+  let mut config: config::Config = serde_json::from_slice(config_json).unwrap();
   config.set_session_id();
 
-  let proving_params = include_bytes!(circuits::PROVING_PARAMS_1024);
+  let proving_params = include_bytes!("../../proofs/web_proof_circuits/circom-artifacts-1024b-v0.8.0/serialized_setup_1024b_rom_length_5.bin");
 
-  let proof = prover_inner(config, Some(proving_params)).await.unwrap();
+  let proof = prover_inner(config, Some(proving_params.to_vec())).await.unwrap();
 
   match proof {
     client::Proof::TLSN(tls_proof) => panic!("expect Origo proof"),
