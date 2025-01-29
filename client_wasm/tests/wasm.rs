c34367389117a1944a3e686fd26cf673160f6b41
mattes <1240531+mattes@users.noreply.github.com>
2025-01-28 16:50:17 -0800

    use include bytes

--- client_wasm/tests/wasm.rs
+++ client_wasm/tests/wasm.rs
@@ -1,16 +1,18 @@
 use client::{
-  circuits, config::{self, NotaryMode}, prover_inner
+  circuits,
+  config::{self, NotaryMode},
+  prover_inner,
 };
 use wasm_bindgen_test::*;
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn test_prover_inner() {
-  let config_json = std::fs::read_to_string("../fixture/client.origo_tcp_local.json").unwrap();
+  let config_json = include_bytes!("../fixture/client.origo_tcp_local.json");
   let mut config: config::Config = serde_json::from_str(&config_json).unwrap();
   config.set_session_id();
 
-  let proving_params = std::fs::read(circuits::PROVING_PARAMS_1024).unwrap();
+  let proving_params = include_bytes!(circuits::PROVING_PARAMS_1024);
 
   let proof = prover_inner(config, Some(proving_params)).await.unwrap();
 
