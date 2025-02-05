5fb98c11fb90e3e716891f26236d433f6c1cfd1d
Colin Roberts <colin@autoparallel.xyz>
2025-02-05 10:53:18 -0700

    WIP: compiling synthesize

--- proofs/src/noir/mod.rs
+++ proofs/src/noir/mod.rs
@@ -9,6 +9,10 @@ use acvm::{
   AcirField,
 };
 use ark_bn254::Fr;
+use bellpepper_core::{
+  num::AllocatedNum, ConstraintSystem, LinearCombination, SynthesisError, Variable,
+};
+use ff::PrimeField;
 
 use super::*;
 
@@ -58,84 +62,126 @@ impl NoirProgram {
     let _status = acvm.solve();
     acvm.finalize()
   }
+
+  pub fn vanilla_synthesize<CS: ConstraintSystem<F<G1>>>(
+    &self,
+    cs: &mut CS,
+    z: &[AllocatedNum<F<G1>>],
+  ) -> Result<Vec<AllocatedNum<F<G1>>>, SynthesisError> {
+    let mut witness_map: HashMap<Witness, AllocatedNum<F<G1>>> = HashMap::new();
+
+    // First, allocate all public inputs
+    let public_inputs = &self.circuit().public_inputs().0;
+    for &witness in public_inputs {
+      // TODO: In Circom, we hold the witness in the circuit struct as an option and match on
+      // whether it is Some or not to get the value we put in here.
+      let var = cs.alloc_input(|| format!("public_{}", witness.as_usize()), || Ok(F::<G1>::ONE));
+      // witness_map.insert(witness, var);
+    }
+
+    // Then, allocate known private witnesses
+    let private_params = &self.circuit().private_parameters;
+    for &witness in private_params {
+      // TODO: In Circom, we hold the witness in the circuit struct as an option and match on
+      // whether it is Some or not to get the value we put in here.
+      let var = cs.alloc(|| format!("private_{}", witness.as_usize()), || Ok(F::<G1>::ONE));
+      // witness_map.insert(witness, var);
+    }
+
+    // c_m0 q_L0 * q_R0 + c_m1 q_L * q_R1 + ... + c_a0 (q_La0 + q_Ra1) + ... + q_c == 0
+    //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ mul        ^^^^^^^^^^^^^ add           ^^ const
+
+    for (gate_idx, opcode) in self.circuit().opcodes.iter().enumerate() {
+      if let Opcode::AssertZero(gate) = opcode {
+        let mut terms = LinearCombination::zero();
+
+        // Handle multiplication terms
+        for (mul_idx, mul_term) in gate.mul_terms.iter().enumerate() {
+          let mut arr = [0u8; 32];
+          arr.copy_from_slice(&mul_term.0.to_be_bytes()[..32]);
+          let coeff = F::<G1>::from_repr(arr).unwrap();
+
+          let left_var = get_or_allocate_var(
+            &mut witness_map,
+            mul_term.1,
+            || format!("mul_{}_left_{}", gate_idx, mul_idx),
+            cs,
+          )?;
+          let right_var = get_or_allocate_var(
+            &mut witness_map,
+            mul_term.2,
+            || format!("mul_{}_right_{}", gate_idx, mul_idx),
+            cs,
+          )?;
+
+          // Create multiplication constraint
+          let mul_result = AllocatedNum::alloc(
+            cs.namespace(|| format!("mul_result_{}_{}", gate_idx, mul_idx)),
+            || Ok(F::<G1>::ONE), // Replace with actual computation when available
+          )?;
+
+          cs.enforce(
+            || format!("mul_constraint_{}_{}", gate_idx, mul_idx),
+            |lc| lc + left_var.get_variable(),
+            |lc| lc + right_var.get_variable(),
+            |lc| lc + mul_result.get_variable(),
+          );
+
+          terms = terms + (coeff, mul_result.get_variable());
+        }
+
+        // Handle linear combinations (add terms)
+        for (add_idx, add_term) in gate.linear_combinations.iter().enumerate() {
+          let mut arr = [0u8; 32];
+          arr.copy_from_slice(&add_term.0.to_be_bytes()[..32]);
+          let coeff = F::<G1>::from_repr(arr).unwrap();
+
+          let var = get_or_allocate_var(
+            &mut witness_map,
+            add_term.1,
+            || format!("add_{}_{}", gate_idx, add_idx),
+            cs,
+          )?;
+          terms = terms + (coeff, var.get_variable());
+        }
+
+        // Add constant term if present
+        if !gate.q_c.is_zero() {
+          let mut arr = [0u8; 32];
+          arr.copy_from_slice(&gate.q_c.to_be_bytes()[..32]);
+          let const_term = F::<G1>::from_repr(arr).unwrap();
+          terms = terms + (const_term, CS::one());
+        }
+
+        // Enforce final constraint: terms = 0
+        cs.enforce(
+          || format!("gate_constraint_{}", gate_idx),
+          |lc| terms.clone(),
+          |lc| lc + CS::one(),
+          |lc| lc,
+        );
+      }
+    }
+
+    Ok(vec![]) // Return appropriate outputs if needed
+  }
 }
 
-// impl ConstraintSynthesizer<Fr> for NoirProgram {
-//   fn generate_constraints(self, cs: ConstraintSystemRef<Fr>) -> ark_relations::r1cs::Result<()> {
-//     let mut witness_map: HashMap<Witness, Variable> = HashMap::new();
-
-//     // First, allocate all public inputs
-//     let public_inputs = &self.circuit().public_inputs().0;
-//     for &witness in public_inputs {
-//       let var = cs.new_input_variable(|| Ok(Fr::ZERO))?;
-//       witness_map.insert(witness, var);
-//     }
-
-//     // Then, allocate known private witnesses
-//     let private_params = &self.circuit().private_parameters;
-//     for &witness in private_params {
-//       let var = cs.new_witness_variable(|| Ok(Fr::ZERO))?;
-//       witness_map.insert(witness, var);
-//     }
-
-//     // c_m0 q_L0 * q_R0 + c_m1 q_L * q_R1 + ... + c_a0 (q_La0 + q_Ra1) + ... + q_c == 0
-//     //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ mul        ^^^^^^^^^^^^^ add           ^^ const
-
-//     for opcode in &self.circuit().opcodes {
-//       if let Opcode::AssertZero(gate) = opcode {
-//         let mut left_terms = LinearCombination::<Fr>::new();
-//         let mut right_terms = LinearCombination::<Fr>::new();
-//         let mut output_terms = LinearCombination::<Fr>::new();
-
-//         // Handle multiplication terms
-//         for mul_term in &gate.mul_terms {
-//           let coeff = Fr::from(mul_term.0.into_repr());
-//           let left_var = allocate_variable(&mut witness_map, mul_term.1, &cs)?;
-//           let right_var = allocate_variable(&mut witness_map, mul_term.2, &cs)?;
-
-//           left_terms += (coeff, left_var);
-//           right_terms += (Fr::ONE, right_var);
-//         }
-
-//         // Handle linear combinations (add terms)
-//         for add_term in &gate.linear_combinations {
-//           let coeff = Fr::from(add_term.0.into_repr());
-//           let var = allocate_variable(&mut witness_map, add_term.1, &cs)?;
-
-//           // Add to the output terms
-//           output_terms += (coeff, var);
-//         }
-
-//         // Add constant term if present
-//         if !gate.q_c.is_zero() {
-//           output_terms += (Fr::from(gate.q_c.into_repr()), Variable::One);
-//         }
-
-//         // The constraint becomes: left_terms * right_terms + output_terms = 0
-//         //                                                         Az o Bz = Cz
-//         cs.enforce_constraint(left_terms, right_terms, -output_terms)?;
-//       }
-//       if let Opcode::MemoryInit { .. } | Opcode::MemoryOp { .. } = opcode {
-//         panic!("Memory Opcode was used! This is not currently supported.");
-//       }
-//     }
-
-//     Ok(())
-//   }
-// }
-
-// // Helper function to allocate variables in the constraint system
-// fn allocate_variable(
-//   witness_map: &mut HashMap<Witness, Variable>,
-//   witness: Witness,
-//   cs: &ConstraintSystemRef<Fr>,
-// ) -> ark_relations::r1cs::Result<Variable> {
-//   if let Some(&var) = witness_map.get(&witness) {
-//     Ok(var)
-//   } else {
-//     // Create a new variable without needing concrete witness values
-//     let var = cs.new_witness_variable(|| Ok(Fr::ZERO))?;
-//     witness_map.insert(witness, var);
-//     Ok(var)
-//   }
-// }
+// Helper function to allocate variables in the constraint system
+fn get_or_allocate_var<CS: ConstraintSystem<F<G1>>>(
+  witness_map: &mut HashMap<Witness, AllocatedNum<F<G1>>>,
+  witness: Witness,
+  ns: impl FnOnce() -> String,
+  cs: &mut CS,
+) -> Result<AllocatedNum<F<G1>>, SynthesisError> {
+  if let Some(var) = witness_map.get(&witness) {
+    Ok(var.clone())
+  } else {
+    let var = AllocatedNum::alloc(
+      cs.namespace(ns),
+      || Ok(F::<G1>::ONE), // Replace with actual witness value
+    )?;
+    witness_map.insert(witness, var.clone());
+    Ok(var)
+  }
+}
