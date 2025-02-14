381c722b3ebc9e6b76eb5ac3d7147bf1d3abd5da
devloper <3347622+devloper@users.noreply.github.com>
2025-02-14 01:32:27 -0500

    fmt

--- client/src/origo_wasm32.rs
+++ client/src/origo_wasm32.rs
@@ -12,33 +12,28 @@ use caratls_ekm_client::DummyTokenVerifier;
 use caratls_ekm_client::TeeTlsConnector;
 #[cfg(feature = "tee-google-confidential-space-token-verifier")]
 use caratls_ekm_google_confidential_space_client::GoogleConfidentialSpaceTokenVerifier;
-use futures::{channel::oneshot, AsyncWriteExt, AsyncReadExt};
+use futures::{channel::oneshot, AsyncReadExt, AsyncWriteExt};
 use hyper::StatusCode;
 use tls_client2::origo::OrigoConnection;
 use tokio_util::compat::TokioAsyncReadCompatExt;
-use wasm_bindgen_futures::spawn_local;
-use ws_stream_wasm::WsMeta;
 use tracing::debug;
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+use web_sys::window;
+use ws_stream_wasm::WsMeta;
 
 use crate::{
   config, config::NotaryMode, errors, errors::ClientErrors, origo::OrigoSecrets,
   tls_client_async2::bind_client, TeeProof,
 };
 
-use wasm_bindgen_futures::JsFuture;
-use web_sys::window;
-
 async fn sleep(ms: u64) {
-    let promise = js_sys::Promise::new(&mut |resolve, _| {
-        window()
-            .unwrap()
-            .set_timeout_with_callback_and_timeout_and_arguments_0(
-                &resolve,
-                ms as i32,
-            )
-            .unwrap();
-    });
-    JsFuture::from(promise).await.unwrap();
+  let promise = js_sys::Promise::new(&mut |resolve, _| {
+    window()
+      .unwrap()
+      .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, ms as i32)
+      .unwrap();
+  });
+  JsFuture::from(promise).await.unwrap();
 }
 
 pub(crate) async fn proxy(
@@ -184,7 +179,7 @@ async fn handle_tee_mode(
   let origo_conn = origo_conn.lock().unwrap().deref().clone();
 
   let (_, mut reunited_socket) = tls_receiver.await.unwrap().unwrap();
-  
+
   let mut buffer = [0u8; 1];
   loop {
     reunited_socket.read_exact(&mut buffer).await?;
