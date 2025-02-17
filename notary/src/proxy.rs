c1d9ec774e2f7e5b943fd623717c755475abce9c
piotr-roslaniec <39299780+piotr-roslaniec@users.noreply.github.com>
2025-02-17 18:50:06 +0100

    feat: Verify manifest, request and response in proxy mode (#472)
    * apply manifest
    
    * check decrypted response with manifest
    
    *  parsing and validation requests
    
    * validate manifest
    
    *  proxy verifies requests and responses
    
    ---------
    
    Co-authored-by: devloper <3347622+devloper@users.noreply.github.com>
    Co-authored-by: Matt <1240531+mattes@users.noreply.github.com>

--- notary/src/proxy.rs
+++ notary/src/proxy.rs
@@ -1,15 +1,20 @@
-use std::{sync::Arc, time::Duration};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use axum::{
   extract::{self, Query, State},
-  response::Response,
   Json,
 };
-use client::{TeeProof, TeeProofData};
+use client::TeeProof;
+use proofs::program::{
+  http::{JsonKey, ManifestRequest, ManifestResponse, ResponseBody},
+  manifest::HTTP_1_1,
+};
+use reqwest::{Request, Response};
 use serde::Deserialize;
-use tracing::info;
+use serde_json::Value;
+use tracing::{debug, info};
 
-use crate::SharedState;
+use crate::{errors::NotaryServerError, SharedState};
 
 #[derive(Deserialize)]
 pub struct NotarizeQuery {
@@ -20,33 +25,87 @@ pub async fn proxy(
   query: Query<NotarizeQuery>,
   State(state): State<Arc<SharedState>>,
   extract::Json(payload): extract::Json<client::ProxyConfig>,
-) -> Json<TeeProof> {
+) -> Result<Json<TeeProof>, NotaryServerError> {
   let session_id = query.session_id.clone();
 
   info!("Starting proxy with ID: {}", session_id);
 
+  let client = reqwest::Client::builder().timeout(Duration::from_secs(30)).build().unwrap();
   let method: reqwest::Method = payload.target_method.parse().unwrap();
 
-  let client = reqwest::Client::builder().timeout(Duration::from_secs(30)).build().unwrap();
   let mut request_builder: reqwest::RequestBuilder = client.request(method, payload.target_url);
-
   for (key, value) in payload.target_headers {
     request_builder = request_builder.header(key, value);
   }
-
   if !payload.target_body.is_empty() {
     request_builder = request_builder.body(payload.target_body.clone());
   }
 
-  let response = request_builder.send().await.unwrap();
+  let reqwest_request = request_builder.try_clone().unwrap().build().unwrap();
+  let reqwest_response = request_builder.send().await.unwrap();
 
-  // TODO
-  // apply manifest to request/ response
+  let request = from_reqwest_request(&reqwest_request);
+  // debug!("{:?}", request);
 
-  let tee_proof = TeeProof {
-    data:      TeeProofData { manifest_hash: "todo".to_string().into_bytes() },
-    signature: "sign(hash(ProxyProofData))".to_string(),
-  };
+  let response = from_reqwest_response(reqwest_response).await;
+  // debug!("{:?}", response);
 
-  Json(tee_proof)
+  if !payload.manifest.request.is_subset_of(&request) {
+    return Err(NotaryServerError::ManifestRequestMismatch);
+  }
+
+  if !payload.manifest.response.is_subset_of(&response) {
+    return Err(NotaryServerError::ManifestResponseMismatch);
+  }
+
+  // TODO: Maybe move that to `TeeProof::from_manifest`?
+  payload.manifest.validate()?;
+
+  let tee_proof = TeeProof::from_manifest(&payload.manifest);
+
+  Ok(Json(tee_proof))
+}
+
+// TODO: This, similarly to other from_* methods, should be a trait
+// Requires adding reqwest to proofs crate
+async fn from_reqwest_response(response: Response) -> ManifestResponse {
+  let status = response.status().as_u16().to_string();
+  let version = format!("{:?}", response.version());
+  let message = response.status().canonical_reason().unwrap_or("").to_string();
+  let headers = response
+    .headers()
+    .iter()
+    .map(|(k, v)| (capitalize_header(k.as_ref()), v.to_str().unwrap_or("").to_string()))
+    .collect();
+  let body: HashMap<String, String> = response.json().await.unwrap_or_default();
+  // TODO: How to handle JsonKey::Num?
+  // TODO Use plain JSON in Manifest etc., and convert to JsonKey as needed
+  let body: Vec<JsonKey> = body.keys().map(|k| JsonKey::String(k.to_string())).collect();
+  ManifestResponse { status, version, message, headers, body: ResponseBody { json: body } }
+}
+
+fn from_reqwest_request(request: &Request) -> ManifestRequest {
+  let method = request.method().to_string();
+  let url = request.url().to_string();
+  let version = format!("{:?}", request.version());
+  let headers = request
+    .headers()
+    .iter()
+    .map(|(k, v)| (capitalize_header(k.as_ref()), v.to_str().unwrap_or("").to_string()))
+    .collect();
+  let body: Value = request.body().map(|b| b.as_bytes().unwrap_or_default().to_vec()).into();
+  ManifestRequest { method, url, version, headers, body: Some(body), vars: Default::default() }
+}
+
+// TODO: Not sure how to normalize data from different formats/protocols into a canonical
+//  `ManifestRequest` and `ManifestResponse`, so for now using this helper as a workaround
+fn capitalize_header(header: &str) -> String {
+  header
+    .split('-')
+    .map(|part| {
+      let mut chars = part.chars();
+      chars.next().map(|c| c.to_ascii_uppercase()).into_iter().chain(chars).collect::<String>()
+    })
+    .collect::<Vec<_>>()
+    .join("-")
 }
