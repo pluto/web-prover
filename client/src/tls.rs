e86342de8e660fe2bdce3a1a8a18d19456a32700
lonerapier <lonerapier@proton.me>
2025-02-22 14:26:58 +0530

    fix: check correct seq when decrypting tls chunk and fix notary ciphertext digest logic

--- client/src/tls.rs
+++ client/src/tls.rs
@@ -1,6 +1,9 @@
 // TODO many root_stores ... this could use some cleanup where possible
 use proofs::program::manifest::{EncryptionInput, TLSEncryption};
-use tls_client2::{origo::WitnessData, CipherSuite, CipherSuiteKey, Decrypter, ProtocolVersion};
+use tls_client2::{
+  origo::{DecryptChunk, WitnessData},
+  CipherSuite, CipherSuiteKey, Decrypter, ProtocolVersion,
+};
 use tls_core::msgs::{base::Payload, enums::ContentType, message::OpaqueMessage};
 use tracing::debug;
 
@@ -95,17 +98,6 @@ pub fn tls_client_default_root_store(
 
 /// Decrypt plaintext from TLS transcript ciphertext using [`WitnessData`]
 pub(crate) fn decrypt_tls_ciphertext(witness: &WitnessData) -> Result<TLSEncryption, ClientErrors> {
-  // Request Preparation
-  let request_key = parse_cipher_key(&witness.request.aead_key)?;
-  let request_iv: [u8; 12] = witness.request.aead_iv[..12].try_into()?;
-  let request_seq = 0;
-  let request_ct = hex::decode(witness.request.ciphertext[0].as_bytes())?;
-  let request_pt = decrypt_chunk(&request_ct, &request_key, request_iv, request_seq)?;
-  let trim_bytes: usize = (TLS_13_TYPE_BYTES + TLS_13_AEAD_BYTES) as usize;
-
-  let mut padded_aad = vec![0; 16 - request_pt.aad.len()];
-  padded_aad.extend(request_pt.aad);
-
   // DEBUG: Use this test case to pin the ciphertext, you must also update
   // `src/notary/origo.rs#verify`.
   //
@@ -125,30 +117,56 @@ pub(crate) fn decrypt_tls_ciphertext(witness: &WitnessData) -> Result<TLSEncrypt
   //   "2c7db2"
   // )).unwrap();
 
-  // TODO (Sambhav): might have to apply similar multi-packet logic for request as well
-  // for now, only support a single request chunk.
-  let request_plaintext = vec![request_pt.plaintext];
-  let request_ciphertext = vec![request_ct[..request_plaintext[0].len()].to_vec()];
-  assert_eq!(request_plaintext[0].len(), request_ciphertext[0].len());
-  debug!("TLS_DECRYPT (request): plaintext={:?}", bytes_to_ascii(request_plaintext[0].clone()));
-  debug!("TLS_DECRYPT (request): ciphertext={:?}", hex::encode(request_ciphertext[0].clone()));
-  debug!(
-    "TLS_DECRYPT (request): trimmed_bytes={:?}",
-    hex::encode(&request_ciphertext[0].clone()[request_ciphertext[0].len() - trim_bytes..])
-  );
+  let trim_bytes: usize = (TLS_13_TYPE_BYTES + TLS_13_AEAD_BYTES) as usize;
+
+  // Request Preparation
+  let request_key = parse_cipher_key(&witness.request.aead_key)?;
+  let request_iv: [u8; 12] = witness.request.aead_iv[..12].try_into()?;
+  let (request_ciphertext, request_plaintext): (Vec<_>, Vec<_>) = witness
+    .request
+    .decrypt_chunk
+    .iter()
+    .enumerate()
+    .map(|(i, DecryptChunk { ciphertext: ct_chunk, seq })| {
+      let ct_chunk = hex::decode(ct_chunk)?;
+      let pt_chunk = decrypt_chunk(&ct_chunk, &request_key, request_iv, *seq)?;
+
+      debug!(
+        "TLS_DECRYPT (request, chunk={:?}): plaintext={:?}",
+        i,
+        bytes_to_ascii(pt_chunk.plaintext.clone())
+      );
+      debug!(
+        "TLS_DECRYPT (request, chunk={:?}): ciphertext={:?}",
+        i,
+        hex::encode(ct_chunk.clone())
+      );
+      debug!(
+        "TLS_DECRYPT (request, chunk={:?}): trimmed_bytes={:?}",
+        i,
+        hex::encode(&ct_chunk.clone()[ct_chunk.len() - trim_bytes..])
+      );
+
+      Ok::<(Vec<u8>, Vec<u8>), ClientErrors>((
+        ct_chunk[..pt_chunk.plaintext.len()].to_vec(),
+        pt_chunk.plaintext,
+      ))
+    })
+    .collect::<Result<Vec<_>, _>>()?
+    .into_iter()
+    .unzip();
 
   // Response Preparation
   let response_key = parse_cipher_key(&witness.response.aead_key)?;
   let response_iv: [u8; 12] = witness.response.aead_iv[..12].try_into()?;
-  let response_seq = request_seq + request_plaintext.len() as u64;
   let (response_ciphertext, response_plaintext): (Vec<_>, Vec<_>) = witness
     .response
-    .ciphertext
+    .decrypt_chunk
     .iter()
     .enumerate()
-    .map(|(i, ct_chunk)| {
+    .map(|(i, DecryptChunk { ciphertext: ct_chunk, seq })| {
       let ct_chunk = hex::decode(ct_chunk)?;
-      let pt_chunk = decrypt_chunk(&ct_chunk, &response_key, response_iv, i as u64 + response_seq)?;
+      let pt_chunk = decrypt_chunk(&ct_chunk, &response_key, response_iv, *seq)?;
 
       debug!(
         "TLS_DECRYPT (response, chunk={:?}): plaintext={:?}",
@@ -179,18 +197,18 @@ pub(crate) fn decrypt_tls_ciphertext(witness: &WitnessData) -> Result<TLSEncrypt
     request:  EncryptionInput {
       key:        request_key,
       iv:         request_iv,
-      aad:        padded_aad.clone(),
+      aad:        vec![0; TLS_13_AEAD_BYTES as usize],
       plaintext:  request_plaintext,
       ciphertext: request_ciphertext,
-      seq:        request_seq,
+      seq:        witness.request.decrypt_chunk[0].seq,
     },
     response: EncryptionInput {
       key:        response_key,
       iv:         response_iv,
-      aad:        padded_aad, // TODO: use response's AAD
+      aad:        vec![0; TLS_13_AEAD_BYTES as usize],
       plaintext:  response_plaintext,
       ciphertext: response_ciphertext,
-      seq:        response_seq,
+      seq:        witness.response.decrypt_chunk[0].seq,
     },
   })
 }
