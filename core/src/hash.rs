2e72b507d050fafd694685e3e8b11c8d07c49347
Piotr Roslaniec <p.roslaniec@gmail.com>
2025-03-13 08:41:58 +0100

    feat: enforce manifest versioning

--- core/src/hash.rs
+++ core/src/hash.rs
@@ -4,7 +4,7 @@ pub fn keccak_digest(bytes: &[u8]) -> [u8; 32] {
   let mut hasher = Keccak::v256();
   let mut output = [0u8; 32];
 
-  hasher.update(&bytes);
+  hasher.update(bytes);
   hasher.finalize(&mut output);
 
   output
